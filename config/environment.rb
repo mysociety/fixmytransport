c29727798edb31b72dc0730495450625043b0f8c
Louise Crow <louise.crow@gmail.com>
2010-04-06 17:07:12 +0100

    Adding dependency on vendor HAML gem

--- config/environment.rb
+++ config/environment.rb
@@ -32,6 +32,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rack", :version => '1.0.1'
+  config.gem "haml", :version => '2.2.20'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
