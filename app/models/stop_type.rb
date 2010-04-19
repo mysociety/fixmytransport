a374890b109ac7a41726015ecae1041eed3d6488
Louise Crow <louise.crow@gmail.com>
2010-04-19 14:59:52 +0100

    Stop type maps type codes to modes of transport

--- app/models/stop_type.rb
+++ app/models/stop_type.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+# Schema version: 20100414172905
+#
+# Table name: stop_types
+#
+#  id                :integer         not null, primary key
+#  code              :string(255)
+#  description       :string(255)
+#  on_street         :boolean
+#  point_type        :string(255)
+#  version           :float
+#  created_at        :datetime
+#  updated_at        :datetime
+#  transport_mode_id :integer
+#
+
 class StopType < ActiveRecord::Base
   belongs_to :transport_mode
+  
+  def self.codes_for_transport_mode(transport_mode_id)
+    stop_types = find_all_by_transport_mode_id(transport_mode_id)
+    stop_types.map{ |stop_type| stop_type.code }
+  end
+  
 end
