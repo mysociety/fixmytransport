45b9a079d6d0b28f5e1a3bce6fded55780690e72
Louise Crow <louise.crow@gmail.com>
2010-04-19 15:49:37 +0100

    Add initial method for figuring out the location of a problem from attributes

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,27 +1,42 @@
 # == Schema Information
-# Schema version: 20100408173847
+# Schema version: 20100414172905
 #
 # Table name: problems
 #
-#  id          :integer         not null, primary key
-#  subject     :text
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
-#  reporter_id :integer
-#  stop_id     :integer
+#  id            :integer         not null, primary key
+#  subject       :text
+#  description   :text
+#  created_at    :datetime
+#  updated_at    :datetime
+#  reporter_id   :integer
+#  stop_area_id  :integer
+#  location_id   :integer
+#  location_type :string(255)
 #
 
 class Problem < ActiveRecord::Base
-  validates_presence_of :subject
-  validates_presence_of :description
+  validates_presence_of :subject, :description, :transport_mode_id, :location
   has_one :reporter, :class_name => 'User'
   accepts_nested_attributes_for :reporter
-  belongs_to :stop
+  belongs_to :location, :polymorphic => true
+  belongs_to :transport_mode
+  attr_accessor :location_attributes
+  before_validation_on_create :location_from_attributes
   
-  def stop_attributes=(attributes)
-    self.stop = Stop.find(:first, :conditions => ['common_name = ? and locality_name = ?', 
-                                  attributes[:common_name], attributes[:locality_name]])
+  def location_from_attributes
+    return unless location_attributes
+    location_attributes[:stop_type_codes] = StopType.codes_for_transport_mode(transport_mode_id)
+    stops = Stop.find_from_attributes(location_attributes)
+    if stops.size == 1  
+      self.location = stops.first
+      return
+    end
+    if stops.size > 1
+      if stop_area = Stop.common_root_area(stops)
+        self.location = stop_area
+        return
+      end
+    end
+    return nil
   end
-  
 end
--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,28 +1,86 @@
 # == Schema Information
-# Schema version: 20100408173847
+# Schema version: 20100414172905
 #
 # Table name: problems
 #
-#  id          :integer         not null, primary key
-#  subject     :text
-#  description :text
-#  created_at  :datetime
-#  updated_at  :datetime
-#  reporter_id :integer
-#  stop_id     :integer
+#  id            :integer         not null, primary key
+#  subject       :text
+#  description   :text
+#  created_at    :datetime
+#  updated_at    :datetime
+#  reporter_id   :integer
+#  stop_area_id  :integer
+#  location_id   :integer
+#  location_type :string(255)
 #
 
 require 'spec_helper'
 
 describe Problem do
+  
   before(:each) do
     @valid_attributes = {
       :subject => "value for subject",
-      :description => "value for description"
+      :description => "value for description",
+      :location => mock_model(Stop),
+      :transport_mode_id => 5
     }
   end
 
   it "should create a new instance given valid attributes" do
-    Problem.create!(@valid_attributes)
+    problem = Problem.new(@valid_attributes)
+    problem.valid?.should be_true
   end
+  
+  it 'should be invalid without a description' do 
+    @valid_attributes[:description] = nil
+    Problem.new(@valid_attributes).valid?.should be_false
+  end
+  
+  it 'should be invalid without a subject' do 
+    @valid_attributes[:subject] = nil
+    Problem.new(@valid_attributes).valid?.should be_false
+  end
+  
+  describe "when finding a location by attributes" do 
+        
+    before do 
+      @problem = Problem.new(@valid_attributes)
+    end
+
+    def expect_location(attributes, location)
+      @problem.location_attributes = attributes
+      @problem.location_from_attributes
+      @problem.location.should == location
+    end
+    
+    it 'should return nil if no location attributes have been set' do 
+      @problem.location_attributes = nil
+      @problem.location_from_attributes.should be_nil
+    end
+    
+    it 'should ask for the stop type codes for the transport mode given' do 
+      StopType.should_receive(:codes_for_transport_mode).with(5).and_return(['TES'])
+      @problem.location_attributes = { :common_name => 'My stop', 
+                                       :locality_name => 'My town' }
+      @problem.location_from_attributes                           
+    end
+    
+    it 'should return a stop if one is uniquely identified by the attributes' do 
+      stop = mock_model(Stop)
+      Stop.stub!(:find_from_attributes).and_return([stop])
+      expect_location({:common_name => 'name', :locality_name => 'locality name'}, stop)
+    end
+    
+    it 'should return a stop area if that stop area is the common root parent of all stops matching the attributes' do 
+      first_stop = mock_model(Stop)
+      second_stop = mock_model(Stop)
+      stop_area = mock_model(StopArea)
+      Stop.stub!(:find_from_attributes).and_return([first_stop, second_stop])
+      Stop.stub!(:common_root_area).and_return(stop_area)
+      expect_location({:common_name => 'name', :locality_name => 'locality name'}, stop_area)
+    end
+    
+  end
+  
 end
