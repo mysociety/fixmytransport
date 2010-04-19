92309a792b1e028cb27a1d8d32b55f7eedd5900c
Louise Crow <louise.crow@gmail.com>
2010-04-19 15:42:26 +0100

    Add patch for making postgres foreign key disabling work for fixtures without a permission hack

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,7 @@
 # Settings specified here will take precedence over those in config/environment.rb
+# An alternative way of disabling foreign keys in fixture loading in Postgres and
+# does not require superuser permissions
+require 'lib/patches/fixtures_constraint_disabling'
 
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
