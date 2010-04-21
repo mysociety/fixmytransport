397dcf5b884663e9fb6ccc59a08027677051d7b3
Louise Crow <louise.crow@gmail.com>
2010-04-21 10:35:03 +0100

    Add function for finding route by number, stops, and mode of transport.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,34 @@
+# == Schema Information
+# Schema version: 20100420165342
+#
+# Table name: routes
+#
+#  id                :integer         not null, primary key
+#  transport_mode_id :integer
+#  number            :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class Route < ActiveRecord::Base
+  has_many :route_operators
+  has_many :operators, :through => :route_operators
+  has_many :route_stops
+  has_many :stops, :through => :route_stops
+  
+  def self.find_existing(attributes)
+    stop_codes = attributes[:stop_codes]
+    routes = self.find(:all, :conditions => ['number = ? and transport_mode_id = ?', 
+                              attributes[:number], attributes[:transport_mode_id]],
+                             :include => { :route_stops => :stop })
+    routes_with_same_stops = []
+    routes.each do |route|
+      route_stop_codes = route.stops.map{ |stop| stop.atco_code }
+      # are the stop code lists identical?
+      if route_stop_codes.size == stop_codes.size && stop_codes.size == (stop_codes & route_stop_codes).size
+        routes_with_same_stops << route
+      end
+    end
+    routes_with_same_stops
+  end
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20100420102749
+#
+# Table name: routes
+#
+#  id                :integer         not null, primary key
+#  transport_mode_id :integer
+#  number            :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 require 'spec_helper'
 
 describe Route do
@@ -11,4 +23,18 @@ describe Route do
   it "should create a new instance given valid attributes" do
     Route.create!(@valid_attributes)
   end
+  
+  describe 'when finding existing routes' do 
+    
+    fixtures :stops, :routes, :route_stops
+  
+    it 'should include in the results returned a route with the same number, mode of transport and stop codes' do 
+      attributes = { :number => '1F50', 
+                     :transport_mode_id => 5, 
+                     :stop_codes => ['9100VICTRIC', '9100CLPHMJC', '9100ECROYDN', '9100GTWK', '9100HYWRDSH'] }
+      Route.find_existing(attributes).include?(routes(:victoria_to_haywards_heath)).should be_true
+    end
+    
+  end
+  
 end
