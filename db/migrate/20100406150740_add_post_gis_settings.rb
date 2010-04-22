fe9c2f6efe1876e510fe92f30fb2889749f75669
Louise Crow <louise.crow@gmail.com>
2010-04-22 15:13:19 +0100

    File name needs to be a param - different on different systems

--- db/migrate/20100406150740_add_post_gis_settings.rb
+++ db/migrate/20100406150740_add_post_gis_settings.rb
@@ -3,6 +3,7 @@ class AddPostGisSettings < ActiveRecord::Migration
   def self.up
     spatial_extensions = MySociety::Config.getbool('USE_SPATIAL_EXTENSIONS', false) 
     sql_file_path = MySociety::Config.get('SPATIAL_EXTENSION_SQL_FILE_PATH', '') 
+    sql_file_name = MySociety::Config.get('SPATIAL_EXTENSION_SQL_FILE_NAME', 'postgis') 
     db_conf = ActiveRecord::Base.configurations[RAILS_ENV]
     postgres = (db_conf['adapter'] == 'postgresql')
     port = db_conf['port']
@@ -10,7 +11,7 @@ class AddPostGisSettings < ActiveRecord::Migration
     username = db_conf['username']
     if spatial_extensions and postgres
       system "createlang -p #{port} plpgsql --username=#{username} #{database}"
-      system "psql -p #{port} -f #{File.join(sql_file_path, 'postgis.sql')} #{database} #{username}"
+      system "psql -p #{port} -f #{File.join(sql_file_path, sql_file_name+'.sql')} #{database} #{username}"
       system "psql -p #{port} -f #{File.join(sql_file_path, 'spatial_ref_sys.sql')} #{database} #{username}"      
     end
   end
