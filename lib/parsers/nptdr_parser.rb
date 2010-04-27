8501efaf6b08f8749d31a7a919bbe857bee1020c
Louise Crow <louise.crow@gmail.com>
2010-04-27 12:56:31 +0100

    Adding route and operator parsing

--- lib/parsers/nptdr_parser.rb
+++ lib/parsers/nptdr_parser.rb
@@ -13,31 +13,74 @@ class Parsers::NptdrParser
       :encoding => 'U' }
   end
   
+  def convert_encoding filepath
+    Iconv.iconv('utf-8', 'WINDOWS-1252', File.read(filepath)).join
+  end
+  
   def vehicle_codes_to_transport_modes(vehicle_code)
     if vehicle_code == 'T'
-      return "train"
-    elif vehicle_code == 'B'
-      return "bus"
-    elif vehicle_code == 'C'
-      return "coach"
-    elif vehicle_code == 'M'
-      return "metro"
-    elif vehicle_code == 'A'
-      return "air"
-    elif vehicle_code == 'F'
-      return "ferry"
+      transport_mode_name = 'Train'
+    elsif vehicle_code == 'B'
+      transport_mode_name = 'Bus'
+    elsif vehicle_code == 'C'
+      transport_mode_name = 'Coach'
+    elsif vehicle_code == 'M'
+      transport_mode_name = 'Metro'
+    elsif vehicle_code == 'A'
+      transport_mode_name = 'Air'
+    elsif vehicle_code == 'F'
+      transport_mode_name = 'Ferry'
     else
-      raise "Unknown vehicle code"
+      raise "Unknown vehicle code '#{vehicle_code}'"
+    end
+    return TransportMode.find_by_name(transport_mode_name)
+  end
+  
+  def parse_operators filepath
+    csv_data = convert_encoding(filepath)
+    FasterCSV.parse(csv_data, csv_options) do |row|
+      yield Operator.new(:code       => row['Operator'].strip, 
+                         :name       => row['Operator Legal Name'].strip,
+                         :short_name => row['Operator Short Form'].strip)
+    end
+  end
+  
+  def parse_stops filepath
+    csv_data = File.read(filepath)
+    FasterCSV.parse(csv_data, csv_options) do |row|
+      yield Stop.new(:atco_code => row['Location Code'],
+                     :common_name => row['Name'], 
+                     :easting => row['Easting'], 
+                     :northing => row['Northing'])
     end
   end
   
   def parse_routes filepath
     csv_data = File.read(filepath)
     FasterCSV.parse(csv_data, csv_options) do |row|
-      vehicle_code = row['Vehicle Code']
-      operator_code = row['Operator Code']
+      route_number = row['Route Number'].strip
+      vehicle_code = row['Vehicle Code'].strip
+      operator_code = row['Operator Code'].strip      
       stop_codes = row['Locations'].split(',')
-      yield Route.new(:number => row['Route Number'].strip)
+      transport_mode = vehicle_codes_to_transport_modes(vehicle_code)
+      route = Route.new(:number => route_number,
+                        :transport_mode => transport_mode)                 
+      stop_codes.each_with_index do |stop_code,index|
+        stop = Stop.find_by_atco_code(stop_code)
+        if ! stop
+          puts "Can't find stop #{stop_code} for route #{route.inspect}" 
+          next
+        end
+        if index == 0 or (index == stop_codes.size - 1)
+          terminus = true
+        else
+          terminus = false
+        end
+        route.route_stops.build(:stop => stop, :terminus => terminus)
+      end
+      operator = Operator.find_or_create_by_code(operator_code)   
+      route.route_operators.build(:operator => operator)                  
+      yield route
     end
   end
 
--- spec/lib/parsers/nptdr_parser_spec.rb
+++ spec/lib/parsers/nptdr_parser_spec.rb
@@ -8,16 +8,91 @@ describe Parsers::NptdrParser do
   
   describe 'when parsing a TSV file of route data' do 
     
-    before(:all) do 
+    before(:each) do 
+      @transport_mode = mock_model(TransportMode)
+      TransportMode.stub!(:find_by_name).with('Bus').and_return(@transport_mode)
+      @stop = mock_model(Stop, :atco_code => 'xxxxx')
+      Stop.stub!(:find_by_atco_code).and_return(@stop)
+      @operator = mock_model(Operator, :code => 'ZZ')
+      Operator.stub!(:find_or_create_by_code).and_return(@operator)
       @parser = Parsers::NptdrParser.new
       @routes = []
-      @parser.parse_routes(example_file("nptdr.tsv")){ |route| @routes << route }
+      @parser.parse_routes(example_file("routes.tsv")){ |route| @routes << route }
+      @route = @routes.first
     end
     
     it 'should extract the route number' do 
-      @routes.first.number.should == '376'
+      @route.number.should == '376'
     end
     
+    it 'should set the transport mode id' do
+      @route.transport_mode_id.should == @transport_mode.id
+    end
+    
+    it 'should add stops to the route' do 
+      @route.route_stops.first.stop.atco_code.should == 'xxxxx'
+      @route.route_stops.second.stop.atco_code.should == 'xxxxx'
+    end
+    
+    it 'should add operators to the route' do 
+      @route.route_operators.first.operator.code.should == 'ZZ'
+    end
+    
+    it 'should mark the route terminus stops' do 
+      @route.route_stops.first.terminus?.should be_true
+      @route.route_stops.last.terminus?.should be_true
+      @route.route_stops.second.terminus?.should be_false
+    end
+    
+    
+  end
+  
+  describe 'when parsing a TSV file of stop data' do 
+
+    before(:all) do 
+      @parser = Parsers::NptdrParser.new
+      @stops = []
+      @parser.parse_stops(example_file("stops.tsv")){ |stop| @stops << stop }
+    end
+   
+    it 'should extract the atco code' do 
+      @stops.first.atco_code.should == '3600XXX'
+    end
+    
+    it 'should extract the common name' do 
+      @stops.first.common_name.should == 'Wells Bus Station'
+    end
+    
+    it 'should extract the easting' do 
+      @stops.first.easting.should == 311122.0
+    end
+    
+    it 'should extract the northing' do 
+      @stops.first.northing.should == 142223.0
+    end
+  
+  end
+  
+  describe 'when parsing a TSV file of operator data' do 
+ 
+    before(:all) do 
+      @parser = Parsers::NptdrParser.new
+      @operators = []
+      @parser.parse_operators(example_file("operators.tsv")){ |operator| @operators << operator }
+    end
+    
+    it 'should extract the operator code' do 
+      @operators.first.code.should == 'WP'
+    end
+    
+    it 'should extract the name' do 
+      @operators.first.name.should == 'Whippet Coaches'
+    end
+    
+    it 'should extract the short name' do 
+      @operators.first.short_name.should == 'Whippet'
+    end
+  
   end
 
 end
