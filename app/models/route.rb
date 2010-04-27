98dda61abbb7c42f37526b8988181a37bdaef348
Louise Crow <louise.crow@gmail.com>
2010-04-27 17:16:52 +0100

    Add Route.add! - a function for adding routes, merging sets of routes where number, transport_mode and at least one location is shared. It also tries to figure out the real terminuses of the route.

--- app/models/route.rb
+++ app/models/route.rb
@@ -12,14 +12,14 @@
 
 class Route < ActiveRecord::Base
   has_many :route_operators
-  has_many :operators, :through => :route_operators
+  has_many :operators, :through => :route_operators, :uniq => true
   has_many :route_stops
-  has_many :stops, :through => :route_stops
+  has_many :stops, :through => :route_stops, :uniq => true, :order => 'common_name asc'
   belongs_to :transport_mode
   validates_presence_of :number
   has_many :problems, :as => :location
   
-  # Are there routes with this number and transport mode that have a stop in common with 
+  # Return routes with this number and transport mode that have a stop in common with 
   # the stop list given
   def self.find_existing(attributes)
     stop_codes = attributes[:stop_codes]
@@ -35,6 +35,41 @@ class Route < ActiveRecord::Base
       end    
     end
     routes_with_same_stops
+  end
+  
+  def self.add!(route)
+    route_attributes = { :number => route.number, 
+                         :transport_mode_id => route.transport_mode.id, 
+                         :stop_codes => route.route_stops.map{ |route_stop| route_stop.stop.atco_code } }
+    existing_routes = find_existing(route_attributes)
+    if existing_routes.empty?
+      route.save!
+      return route
+    end
+    original = existing_routes.first
+    duplicates = existing_routes - [original] 
+    duplicates += [route]
+    duplicates.each do |duplicate|
+      if !duplicate.problems.empty?
+        raise "Can't merge route with problems: #{duplicates.inspect}"
+      end
+      duplicate.route_operators.each do |route_operator|
+        if ! original.route_operators.detect { |existing| existing.operator == route_operator.operator }
+          original.route_operators << route_operator
+        end
+      end
+      duplicate.route_stops.each do |route_stop|
+        if existing = original.route_stops.detect { |existing| existing.stop == route_stop.stop }
+          if existing.terminus? and !route_stop.terminus?
+            existing.terminus = false
+          end
+        else
+          original.route_stops << route_stop
+        end
+      end
+      duplicate.destroy
+    end
+    original.save!
   end
 
   def transport_mode_name
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -28,8 +28,87 @@ describe Route do
     route.valid?.should be_true
   end
   
-  describe 'when finding existing routes' do 
+  describe 'when adding a route' do 
+  
+    it 'should raise an exception if a route to be merged has problems associated with it' do
+      Route.stub!(:find_existing).and_return([mock_model(Route)])
+      route = Route.new(:problems => [mock_model(Problem)], :transport_mode_id => 5, :number => '43')
+      lambda{ Route.add!(route) }.should raise_exception(/Can't merge route with problems/)
+    end
     
+    it 'should save the route if no existing routes are found' do 
+      Route.stub!(:find_existing).and_return([])
+      route = Route.new(:transport_mode_id => 5, :number => '43')
+      route.should_receive(:save!)
+      Route.add!(route)
+    end
+    
+    it 'should transfer route operator associations when merging overlapping routes' do 
+      existing_route_operator = mock_model(RouteOperator, :operator => mock_model(Operator))
+      existing_route = mock_model(Route, :save! => true, :route_operators => [existing_route_operator])
+      Route.stub!(:find_existing).and_return([existing_route])
+      route_operator = mock_model(RouteOperator, :operator => mock_model(Operator))
+      route = Route.new(:transport_mode_id => 5, 
+                        :number => '43', 
+                        :route_operators => [route_operator])
+      Route.add!(route)
+      existing_route.route_operators.should == [existing_route_operator, route_operator]
+    end 
+    
+    it 'should not add duplicate route operator associations when merging overlapping routes' do 
+      operator = mock_model(Operator)
+      route_operator = mock_model(RouteOperator, :operator => operator)
+      existing_route = mock_model(Route, :save! => true, :route_operators => [route_operator])
+      Route.stub!(:find_existing).and_return([existing_route])
+      
+      route = Route.new(:transport_mode_id => 5, 
+                        :number => '43', 
+                        :route_operators => [route_operator])
+      Route.add!(route)
+      existing_route.route_operators.should == [route_operator]
+    end
+    
+    it 'should transfer route stops when merging overlapping routes' do 
+      existing_route_stop = mock_model(RouteStop, :stop => mock_model(Stop))
+      existing_route = mock_model(Route, :save! => true, :route_stops => [existing_route_stop])
+      Route.stub!(:find_existing).and_return([existing_route])
+      route_stop = mock_model(RouteStop, :stop => mock_model(Stop, :atco_code => 'aaaa'))
+      route = Route.new(:transport_mode_id => 5, 
+                        :number => '43', 
+                        :route_stops => [route_stop])
+      Route.add!(route)
+      existing_route.route_stops.should == [existing_route_stop, route_stop]
+    end
+    
+    it 'should not add duplicate route stops when merging overlapping routes' do 
+      existing_stop = mock_model(Stop, :atco_code => 'aaaaaa')
+      existing_route_stop = mock_model(RouteStop, :stop => existing_stop, :terminus? => false)
+      existing_route = mock_model(Route, :save! => true, :route_stops => [existing_route_stop])
+      Route.stub!(:find_existing).and_return([existing_route])
+      route_stop = mock_model(RouteStop, :stop => existing_stop)
+      route = Route.new(:transport_mode_id => 5, 
+                        :number => '43', 
+                        :route_stops => [route_stop])
+      Route.add!(route)
+      existing_route.route_stops.should == [existing_route_stop]
+    end
+    
+    it 'should not make as terminuses stops that are terminuses in an existing route but not terminuses in a duplicate' do 
+      existing_stop = mock_model(Stop, :atco_code => 'aaaaaa')
+      existing_route_stop = mock_model(RouteStop, :stop => existing_stop, :terminus? => true)
+      existing_route = mock_model(Route, :save! => true, :route_stops => [existing_route_stop])
+      Route.stub!(:find_existing).and_return([existing_route])
+      route_stop = mock_model(RouteStop, :stop => existing_stop, :terminus? => false)
+      route = Route.new(:transport_mode_id => 5, 
+                        :number => '43', 
+                        :route_stops => [route_stop])
+      existing_route_stop.should_receive(:terminus=).with(false)
+      Route.add!(route)
+    end
+    
+  end
+  
+  describe 'when finding existing routes' do 
   
     it 'should include in the results returned a route with the same number, mode of transport and stop codes' do 
       attributes = { :number => '1F50', 
