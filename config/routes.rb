74f44c7fe77b7d70b4fc0afe4932b0c1b63e5c6c
Louise Crow <louise.crow@gmail.com>
2010-04-29 20:05:59 +0100

    Random routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
   map.resources :problems, :except => [:update, :edit]
-  map.resources :routes, :only => [:show]
-  map.resources :stops, :only => [:show ]
+  map.resources :routes, :only => [:show], :collection => {:random => :get}
+  map.resources :stops, :only => [:show], :collection => {:random => :get}
+  map.resources :stop_areas, :only => [:show], :collection => {:random => :get}
 end
