61c4e3ade8a90a9f807e22f152bb115cecf6cb33
Louise Crow <louise.crow@gmail.com>
2010-05-05 16:10:14 +0100

    Some URL helpers to get around the limitations of polymorphic_url, a location_search function to prevent multiple calls to the finder in a request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,32 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  helper_method :location_search, :location_url, :respond_url
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  private
+  
+  def location_search 
+    @location_search ||= LocationSearch.find_current(session[:session_id])
+  end
+   
+  def location_url(location)
+   location = location.becomes(Route) if location.is_a? Route 
+   polymorphic_url(location) 
+  end
+
+  def respond_url(location, params)
+   case location
+   when Route
+     respond_route_url(location, params)
+   when Stop
+     respond_stop_url(location, params)
+   when StopArea
+     respond_stop_area_url(location, params)
+   else
+     raise "Unknown location type: #{location.class}"
+   end
+  end
+  
 end
