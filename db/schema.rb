b5987ced2efd6831607b7eff98d96c03c3635d18
Louise Crow <louise.crow@gmail.com>
2010-05-05 16:02:40 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100504162509) do
+ActiveRecord::Schema.define(:version => 20100505100610) do
+
+  create_table "location_searches", :force => true do |t|
+    t.integer  "transport_mode_id"
+    t.string   "name"
+    t.string   "area"
+    t.string   "route_number"
+    t.string   "location_type"
+    t.string   "session_id"
+    t.text     "events"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "operators", :force => true do |t|
     t.string   "code"
@@ -60,6 +73,16 @@ ActiveRecord::Schema.define(:version => 20100504162509) do
   add_index "routes", ["number"], :name => "index_routes_on_number"
   add_index "routes", ["transport_mode_id"], :name => "index_routes_on_transport_mode_id"
   add_index "routes", ["type"], :name => "index_routes_on_type"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "stop_area_links", :force => true do |t|
     t.integer  "ancestor_id"
