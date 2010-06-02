eb96e3d7430652c2a030a9684daf6f0b382ff2ac
Louise Crow <louise.crow@gmail.com>
2010-06-02 19:38:34 +0100

    Use name when finding stops, don't require area for routes.

--- app/models/gazetteer.rb
+++ app/models/gazetteer.rb
@@ -4,13 +4,18 @@ class Gazetteer
     return [] if attributes[:area].blank?
     stop_type_codes = StopType.codes_for_transport_mode(attributes[:transport_mode_id])
     localities = Locality.find_all_with_descendants(attributes[:area])
-    stops = Stop.find(:all, :conditions => ['locality_id in (?) and stop_type in (?)', 
-                            localities, stop_type_codes], :limit => limit)
+    query = 'locality_id in (?) and stop_type in (?)'
+    params = [localities, stop_type_codes]
+    if !attributes[:name].blank? 
+      query += ' AND lower(common_name) like ?'
+      params <<  "%#{attributes[:name].downcase}%"
+    end
+    conditions = [query] + params
+    stops = Stop.find(:all, :conditions => conditions, :limit => limit)
     stops
   end
   
   def self.find_routes_from_attributes(attributes, limit=nil)
-    return [] if attributes[:area].blank?
     localities = Locality.find_all_with_descendants(attributes[:area])
     routes = Route.find_all_by_transport_mode_id(attributes[:transport_mode_id], 
                                                  route_number=attributes[:route_number], 
