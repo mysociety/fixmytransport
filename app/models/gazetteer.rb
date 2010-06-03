988b2175f09c2e0cbc9be57e0757d7cce759facc
Louise Crow <louise.crow@gmail.com>
2010-06-03 15:31:26 +0100

    Area can be blank when doing gazetteer searches

--- app/models/gazetteer.rb
+++ app/models/gazetteer.rb
@@ -1,11 +1,14 @@
 class Gazetteer 
 
   def self.find_stops_from_attributes(attributes, limit=nil)
-    return [] if attributes[:area].blank?
     stop_type_codes = StopType.codes_for_transport_mode(attributes[:transport_mode_id])
-    localities = Locality.find_all_with_descendants(attributes[:area])
-    query = 'locality_id in (?) AND stop_type in (?)'
-    params = [localities, stop_type_codes]
+    query = 'stop_type in (?)'
+    params = [stop_type_codes]
+    if !attributes[:area].blank?
+      localities = Locality.find_all_with_descendants(attributes[:area])
+      query += ' AND locality_id in (?)'
+      params << localities
+    end
     if !attributes[:name].blank? 
       name = attributes[:name].downcase
       query += ' AND (lower(common_name) like ? OR lower(street) = ? OR naptan_code = ?)'
@@ -19,7 +22,10 @@ class Gazetteer
   end
   
   def self.find_routes_from_attributes(attributes, limit=nil)
-    localities = Locality.find_all_with_descendants(attributes[:area])
+    localities = []
+    if !attributes[:area].blank?
+      localities = Locality.find_all_with_descendants(attributes[:area])
+    end
     routes = Route.find_all_by_transport_mode_id(attributes[:transport_mode_id], 
                                                  route_number=attributes[:route_number], 
                                                  localities=localities, 
@@ -27,4 +33,5 @@ class Gazetteer
     routes
   end
   
+
 end
