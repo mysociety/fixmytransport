739a126741e9afdb3f3a0dc7c524d332fbe4c0e2
Louise Crow <louise.crow@gmail.com>
2010-06-03 15:32:09 +0100

    Adding 'will_paginate' gem to requirements

--- config/environment.rb
+++ config/environment.rb
@@ -36,6 +36,8 @@ Rails::Initializer.run do |config|
   config.gem "haml", :version => '2.2.20'
   config.gem "erubis", :version => '2.6.5'
   config.gem "fastercsv", :version => '1.5.3'
+  config.gem 'will_paginate', :version => '2.3.12'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
