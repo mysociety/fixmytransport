c92c6b71966fd88db9b538ce0e517e63e7eb1bf5
Louise Crow <louise.crow@gmail.com>
2010-06-07 19:15:52 +0100

    Email confirmation route, condense resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,13 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :stories, :controller => 'problems', 
                           :except => [:update, :edit, :create], 
                           :collection => {:choose_location => :get, :find => :post, :recent => :get}
+  map.confirm '/c/:email_token', :action => 'confirm', :controller => 'problems'
 
-  map.resources :routes, :only => [:show], 
-                         :collection => {:random => :get}, 
-                         :member => {:respond => :get}
-
-  map.resources :stops, :only => [:show], 
-                        :collection => {:random => :get}, 
-                        :member => {:respond => :get}
-
-  map.resources :stop_areas, :only => [:show], 
-                             :collection => {:random => :get}, 
-                             :member => {:respond => :get}
+  [:routes, :stops, :stop_areas].each do |location|
+    map.resources location, :only => [:show, :update], 
+                            :collection => {:random => :get}, 
+                            :member => {:respond => :get}
+  end
 
   # static
   map.about '/about', :controller => 'static', :action => 'about'
