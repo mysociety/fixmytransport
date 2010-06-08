5c413145502d386c9eb25d5dade82309eb4904cd
Louise Crow <louise.crow@gmail.com>
2010-06-08 14:39:56 +0100

    Ooops one of these isn't removed

--- db/migrate/20100603100029_remove_locality_names_from_stops.rb
+++ db/migrate/20100603100029_remove_locality_names_from_stops.rb
@@ -9,7 +9,6 @@ class RemoveLocalityNamesFromStops < ActiveRecord::Migration
     add_column :stops, :grand_parent_locality_name, :string
     add_column :stops, :locality_name, :string
     add_column :stops, :parent_locality_name, :string
-    execute "CREATE INDEX index_stops_on_common_name_lower ON stops ((lower(common_name)));"
     execute "CREATE INDEX index_stops_on_locality_name_lower ON stops ((lower(locality_name)));"
     execute "CREATE INDEX index_stops_on_parent_locality_name_lower ON stops ((lower(parent_locality_name)));"
     execute "CREATE INDEX index_stops_on_grand_parent_locality_name_lower ON stops ((lower(grand_parent_locality_name)));"
