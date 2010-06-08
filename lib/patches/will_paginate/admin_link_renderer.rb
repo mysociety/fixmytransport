ff7ab7b954f9c90f6ced07c08c050d0638a258ce
Louise Crow <louise.crow@gmail.com>
2010-06-08 09:49:25 +0100

    Admin link renderer gets option indicating whether to render links to the admin interface or the main interface

--- lib/patches/will_paginate/admin_link_renderer.rb
+++ lib/patches/will_paginate/admin_link_renderer.rb
@@ -3,8 +3,14 @@ module WillPaginate
     
     private 
     
+    # If the admin option is set, links point to the admin interface
     def page_link(page, text, attributes = {})
-       @template.link_to text, @template.admin_url(url_for(page)), attributes
+      if @options[:admin]
+        url = @template.admin_url(url_for(page))
+      else
+        url = @template.main_url(url_for(page))
+      end
+      @template.link_to text, url, attributes
     end
     
   end
