b76aa68d9df0908af2ca1c79575100589bc1b491
Louise Crow <louise.crow@gmail.com>
2010-06-21 11:42:31 +0100

    Recast route before updated to preserve errors.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,12 +2,14 @@ class RoutesController < LocationsController
   
   def show
     @route = Route.find(params[:id], :scope => params[:scope])
+    @route = @route.becomes(Route)
     @new_story = Story.new(:reporter => User.new)
     location_search.add_location(@route) if location_search
   end
   
   def update
     @route = Route.find(params[:id], :scope => params[:scope])
+    @route = @route.becomes(Route)
     update_location(@route, params[:route])
   end
 
