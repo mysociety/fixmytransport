65fc471b7e809bb233ebf8d016ced9de0f68d827
Louise Crow <louise.crow@gmail.com>
2010-06-21 10:09:40 +0100

    Renaming autocreated sequence manually when renaming table

--- db/migrate/20100610153811_rename_problems_to_stories.rb
+++ db/migrate/20100610153811_rename_problems_to_stories.rb
@@ -1,9 +1,11 @@
 class RenameProblemsToStories < ActiveRecord::Migration
   def self.up
     rename_table :problems, :stories
+    execute "ALTER TABLE problems_id_seq RENAME TO stories_id_seq"
   end
 
   def self.down
     rename_table :stories, :problems
+    execute "ALTER TABLE stories_id_seq RENAME TO problems_id_seq"
   end
 end
