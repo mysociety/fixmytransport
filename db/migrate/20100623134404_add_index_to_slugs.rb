13221e8f517fe99127b0904d3fb6f0b06407ff08
Louise Crow <louise.crow@gmail.com>
2010-06-24 18:25:51 +0100

    Bugfix on index name

--- db/migrate/20100623134404_add_index_to_slugs.rb
+++ db/migrate/20100623134404_add_index_to_slugs.rb
@@ -1,6 +1,6 @@
 class AddIndexToSlugs < ActiveRecord::Migration
   def self.up 
-    add_index :slugs, [:sluggable_id, :sluggable_type],  :name => 'index_stops_on_sluggable_id_and_sluggable_type'
+    add_index :slugs, [:sluggable_id, :sluggable_type],  :name => 'index_slugs_on_sluggable_id_and_sluggable_type'
   end
 
   def self.down
