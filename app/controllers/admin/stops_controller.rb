9f873f627e2492ad3ecd83ffc5cf8a0823a0b5a8
Louise Crow <louise.crow@gmail.com>
2010-07-06 15:41:15 +0100

    Translation, style edits

--- app/controllers/admin/stops_controller.rb
+++ app/controllers/admin/stops_controller.rb
@@ -6,8 +6,13 @@ class Admin::StopsController < ApplicationController
     query = params[:term].downcase
     stops = Stop.find_by_name_or_id(query, params[:transport_mode_id], 20)
     stops = stops.map do |stop| 
+      name = stop.full_name
+      if ! stop.street.blank?
+        name += " #{t(:on_street, :street => stop.street)}"
+      end 
+      name += " #{t(:in_locality, :locality => stop.locality_name)} (#{stop.id})"
       { :id => stop.id, 
-        :name => "#{stop.full_name} #{t(:on)} #{stop.street} #{t(:in)} #{stop.locality_name} (#{stop.id})" } 
+        :name => name } 
     end
     render :json => stops
   end
