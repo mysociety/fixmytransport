b5e246a8875a701cd19e18d63588042f70d7b5bd
Louise Crow <louise.crow@gmail.com>
2010-08-18 14:30:53 +0100

    Fixing failing spec

--- spec/models/passenger_transport_executive_areas_spec.rb
+++ spec/models/passenger_transport_executive_areas_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe PassengerTransportExecutiveAreas do
+describe PassengerTransportExecutiveArea do
   before(:each) do
     @valid_attributes = {
       :area_id => 1,
-      :passenger_transport_executive => 1
+      :passenger_transport_executive_id => 1
     }
   end
 
   it "should create a new instance given valid attributes" do
-    PassengerTransportExecutiveAreas.create!(@valid_attributes)
+    PassengerTransportExecutiveArea.create!(@valid_attributes)
   end
 end
