f436b874169c8e9493ed7b550c567b03c8e9f39f
Louise Crow <louise.crow@gmail.com>
2010-08-19 19:06:46 +0100

    Bugfix

--- db/migrate/20100817193959_add_ptes_to_problems.rb
+++ db/migrate/20100817193959_add_ptes_to_problems.rb
@@ -1,9 +1,9 @@
 class AddPtesToProblems < ActiveRecord::Migration
   def self.up
-    add_column :problems, :public_transport_executive_id, :integer
+    add_column :problems, :passenger_transport_executive_id, :integer
   end
 
   def self.down
-    remove_column :problems, :public_transport_executive_id
+    remove_column :problems, :passenger_transport_executive_id
   end
 end
