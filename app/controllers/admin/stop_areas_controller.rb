02cdfff3b8beebeb53b1f9aa99a97fb9d60c80cb
Louise Crow <louise.crow@gmail.com>
2010-09-02 16:13:30 +0100

    Missing files from previous commit

---
+++ app/controllers/admin/stop_areas_controller.rb
@@ -0,0 +1,70 @@
+class Admin::StopAreasController < ApplicationController
+  
+  layout "admin" 
+  cache_sweeper :stop_area_sweeper, :only => [:create, :update, :destroy]
+  
+  def show 
+    @stop_area = StopArea.find(params[:id])
+  end
+  
+  def index
+    conditions = []
+    if !params[:query].blank? or !params[:mode].blank?
+      conditions = StopArea.name_or_id_conditions(params[:query], params[:mode])
+    end
+    @stop_areas = StopArea.paginate :page => params[:page], 
+                                    :conditions => conditions, 
+                                    :order => 'name'
+  end
+  
+  def new
+    @stop_area = StopArea.new(:loaded => true)
+  end
+  
+  def create 
+    @stop_area = StopArea.new(params[:stop_area])
+    if @stop_area.save
+      flash[:notice] = t(:stop_area_created)
+      redirect_to(admin_url(admin_stop_area_path(@stop_area.id)))
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @stop_area = StopArea.find(params[:id])
+    if @stop_area.update_attributes(params[:stop_area])
+      flash[:notice] = t(:stop_area_updated)
+      redirect_to admin_url(admin_stop_area_path(@stop_area.id))
+    else
+      flash[:error] = t(:stop_area_problem)
+      render :show
+    end
+  end
+  
+  def destroy
+    @stop_area = StopArea.find(params[:id])
+    if @stop_area.campaigns.size > 0
+      flash[:error] = t(:stop_area_has_campaigns)
+      render :show
+    elsif @stop_area.routes.size > 0
+      flash[:error] = t(:stop_area_has_routes)
+      render :show
+    else
+      @stop_area.destroy
+      flash[:notice] = t(:stop_area_destroyed)
+      redirect_to admin_url(admin_stop_areas_path)
+    end
+  end
+  
+  def autocomplete_for_name
+    query = params[:term].downcase
+    stop_areas = StopArea.find_by_name_or_id(query, params[:transport_mode_id], 20)
+    stop_areas = stop_areas.map do |stop_area| 
+      { :id => stop_area.id, 
+        :name => @template.stop_area_name_for_admin(stop_area) } 
+    end
+    render :json => stop_areas
+  end
+
+end
