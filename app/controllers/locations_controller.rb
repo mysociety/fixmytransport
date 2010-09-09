d8f92dbbf42dd9ddfaa8e3137557bf086759382f
Louise Crow <louise.crow@gmail.com>
2010-09-09 18:15:17 +0100

    Consolidate these location views into one controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,68 @@
+class LocationsController < ApplicationController
+  
+  before_filter :process_map_params, :except => [:in_area]
+  
+  def show_stop
+    @stop = Stop.full_find(params[:id], params[:scope])
+    @title = @stop.full_name
+    if location_search
+      location_search.add_location(@stop) 
+    end
+    respond_to do |format|
+      format.html do 
+        map_params_from_location(@stop.points, find_other_locations=true)
+      end
+      format.atom do  
+        @campaigns = @stop.campaigns.confirmed
+        render :template => 'shared/campaigns.atom.builder', :layout => false 
+      end
+    end
+  end
+  
+  def show_stop_area
+    @stop_area = StopArea.full_find(params[:id], params[:scope])
+
+    # redirect to a station/ferry terminal url if appropriate
+    if params[:type] != :station && StopAreaType.station_types.include?(@stop_area.area_type)
+      redirect_to @template.location_url(@stop_area) and return false
+    end
+    if params[:type] != :ferry_terminal && StopAreaType.ferry_terminal_types.include?(@stop_area.area_type)
+      redirect_to @template.location_url(@stop_area) and return false
+    end
+    
+    @title = @stop_area.name
+    if location_search
+      location_search.add_location(@stop_area) 
+    end
+    respond_to do |format|
+      format.html do
+        map_params_from_location(@stop_area.points, find_other_locations=true)
+      end
+      format.atom do  
+        @campaigns = @stop_area.campaigns.confirmed
+        render :template => 'shared/campaigns.atom.builder', :layout => false 
+      end
+    end
+  end
+  
+  def show_route
+    @route = Route.full_find(params[:id], params[:scope])
+    location_search.add_location(@route) if location_search
+    @title = @route.name
+    respond_to do |format|
+      format.html do 
+        map_params_from_location(@route.points, find_other_locations=false)
+      end
+      format.atom do  
+        @campaigns = @route.campaigns.confirmed
+        render :template => 'shared/campaigns.atom.builder', :layout => false 
+      end
+    end
+  end
+  
+  def in_area
+    other_locations =  Map.other_locations(params[:lat].to_f, params[:lon].to_f, params[:zoom].to_i)
+    render :json => "#{@template.location_stops_js(other_locations, main=false, small=true)}"
+  end
+
+end
---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe LocationsController do
+
+  describe 'GET #show_stop_area' do
+    
+    def make_request
+      get :show_stop_area, { :type => :stop_area, :scope => 'london', :id => 'euston' }
+    end
+    
+    it 'should redirect to a station url if the stop area is a train station' do 
+      mock_stop_area = mock_model(StopArea, :area_type => 'GRLS', :locality => 'london')
+      StopArea.stub!(:full_find).and_return(mock_stop_area)
+      make_request
+      response.should redirect_to(station_url(mock_stop_area.locality, mock_stop_area))
+    end
+    
+    it 'should redirect to a station url if the stop area is a metro station' do
+      mock_stop_area = mock_model(StopArea, :area_type => 'GTMU', :locality => 'london')
+      StopArea.stub!(:full_find).and_return(mock_stop_area)
+      make_request
+      response.should redirect_to(station_url(mock_stop_area.locality, mock_stop_area))
+    end
+    
+    it 'should redirect to a ferry terminal url if the stop area is a ferry terminal' do 
+      mock_stop_area = mock_model(StopArea, :area_type => 'GFTD', :locality => 'london')
+      StopArea.stub!(:full_find).and_return(mock_stop_area)
+      make_request
+      response.should redirect_to(ferry_terminal_url(mock_stop_area.locality, mock_stop_area))
+    end
+    
+  end
+end
