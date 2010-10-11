0c2fc435c530f746abd74422f29d4f75045acb64
Louise Crow <louise.crow@gmail.com>
2010-10-11 18:13:04 +0100

    Add redirect param to login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,6 +3,9 @@ class UserSessionsController < ApplicationController
   before_filter :require_user, :only => :destroy
 
   def new
+    if params[:redirect] and params[:redirect].starts_with('/')
+      session[:return_to] = params[:redirect]
+    end
     @user_session = UserSession.new
   end
 
