fb8b711f542858e307f54bc2d9b708dd86d0c54a
Louise Crow <louise.crow@gmail.com>
2010-10-25 08:52:57 +0100

    Handle council contact emails too

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -100,7 +100,14 @@ class IncomingMessage < ActiveRecord::Base
   # Converts email addresses we know about into textual descriptions of them
   def mask_special_emails(text)
     campaign.problem.emailable_organizations.each do |organization|
-      text.gsub!(organization.email, "[#{organization.name} problem reporting email]")
+      if organization.is_a? Council
+        emails = [organization.email_for_category(campaign.problem.category)]
+      else
+        email = organization.emails 
+      end
+      emails.each do |email|
+        text.gsub!(email, "[#{organization.name} problem reporting email]")
+      end
     end
     campaign.valid_local_parts.each do |local_part|
       text.gsub!("#{local_part}@#{campaign.domain}", "[#{campaign.title} email]")
