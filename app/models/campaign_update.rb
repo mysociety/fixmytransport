c6fa161446b19ea67568af476e72aa42fb75fbf3
Louise Crow <louise.crow@gmail.com>
2011-02-10 20:44:25 +0000

    Order of comments on update

--- app/models/campaign_update.rb
+++ app/models/campaign_update.rb
@@ -4,7 +4,7 @@ class CampaignUpdate < ActiveRecord::Base
   belongs_to :incoming_message
   belongs_to :outgoing_message
   validates_presence_of :text
-  has_many :comments, :as => :commented
+  has_many :comments, :as => :commented, :order => 'confirmed_at asc'
   has_many :campaign_events, :as => :described
   named_scope :general, :conditions => ['incoming_message_id is null and outgoing_message_id is null']
   named_scope :unsent, :conditions => ['sent_at is null']
