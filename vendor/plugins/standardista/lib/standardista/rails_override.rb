75e7f079134433282c6722a90681c9979b2bbaf1
Louise Crow <louise.crow@gmail.com>
2011-02-14 18:49:11 +0000

    Plugin update to work with html_safe

--- vendor/plugins/standardista/lib/standardista/rails_override.rb
+++ vendor/plugins/standardista/lib/standardista/rails_override.rb
@@ -2,7 +2,8 @@ module Standardista
   module TagHelper
     # override tag helper from Rails to disable self-closing tags
     def tag(name, options = nil, open = false, escape = true)
-      "<#{name}#{tag_options(options, escape) if options}>"
+      tag_string = "<#{name}#{tag_options(options, escape) if options}>"
+      tag_string.respond_to?('html_safe') ? tag_string.html_safe : tag_string
     end
   end
 end
@@ -12,6 +13,7 @@ ActionView::Base.send :include, Standardista::TagHelper
 # unfortunately, this duplication seems necessary :(
 ActionView::Helpers::InstanceTag.class_eval do
   def tag_without_error_wrapping(name, options = nil, open = false, escape = true)
-    "<#{name}#{tag_options(options, escape) if options}>"
+    tag_string = "<#{name}#{tag_options(options, escape) if options}>"
+    tag_string.respond_to?('html_safe') ? tag_string.html_safe : tag_string
   end
 end
