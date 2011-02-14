dbb05ed5bd9cc880265e93a32f31d2b184295a29
Louise Crow <louise.crow@gmail.com>
2011-02-14 17:58:41 +0000

    New rails version, gem versions, update load_path syntax

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -32,23 +32,25 @@ Rails::Initializer.run do |config|
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/app/sweepers )
+  config.autoload_paths += %W( #{RAILS_ROOT}/app/sweepers )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "rack", :version => '1.0.1'
-  config.gem "haml", :version => '2.2.20'
-  config.gem "erubis", :version => '2.6.5'
+  config.gem "rack", :version => '1.1.0'
+  config.gem "haml", :version => '3.0.25'
+  config.gem "erubis", :version => '2.6.6'
   config.gem "fastercsv", :version => '1.5.3'
-  config.gem 'will_paginate', :version => '2.3.12'
+  config.gem 'will_paginate', :version => '2.3.15'
   config.gem "friendly_id", :version => '3.1.7'
   config.gem 'paper_trail', :version => '1.5.1'
   config.gem 'authlogic', :version => '2.1.6'
   config.gem 'session', :version => '3.1.0'
   config.gem 'text', :version => '0.2.0'
+  config.gem 'rspec', :lib => false, :version => '1.3.1'
+  config.gem 'rspec-rails', :lib => false, :version => '1.3.3'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
