9e5db21e9b4a28b1b6c7eef506d23527aedb9244
Louise Crow <louise.crow@gmail.com>
2011-03-09 14:00:25 +0000

    Update spec for new data format.

--- spec/models/alternative_name_spec.rb
+++ spec/models/alternative_name_spec.rb
@@ -23,12 +23,8 @@ require 'spec_helper'
 describe AlternativeName do
   before(:each) do
     @valid_attributes = {
-      :name => "value for name",
+      :alternative_locality_id => 2,
       :locality_id => 1,
-      :short_name => "value for short_name",
-      :qualifier_name => "value for qualifier_name",
-      :qualifier_locality => "value for qualifier_locality",
-      :qualifier_district => "value for qualifier_district",
       :creation_datetime => Time.now,
       :modification_datetime => Time.now,
       :revision_number => "value for revision_number",
