7d1cbc91a0fa39b18b1ee100c39c944a5a3e33ec
Louise Crow <louise.crow@gmail.com>
2011-03-14 16:48:33 +0000

    Updating spec for journey patterns

--- spec/route_spec_helper.rb
+++ spec/route_spec_helper.rb
@@ -1,8 +1,9 @@
 def add_stops_from_list route, atco_codes
+  jp = route.journey_patterns.build(:destination => 'test dest')
   atco_codes.each_cons(2) do |from_atco_code, to_atco_code|
     from_stop = get_stop(from_atco_code)
     to_stop = get_stop(to_atco_code)
-    route.route_segments << RouteSegment.new(:from_stop => from_stop, :to_stop => to_stop)
+    jp.route_segments << RouteSegment.new(:from_stop => from_stop, :to_stop => to_stop)
   end
 end
 
