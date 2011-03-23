8a07fad46aafd71f75985319b058f98e8106adb4
Louise Crow <louise.crow@gmail.com>
2011-03-23 18:17:46 +0000

    Handle special case for Essex council

--- lib/parsers/fms_contacts_parser.rb
+++ lib/parsers/fms_contacts_parser.rb
@@ -1,27 +1,45 @@
 # Parser for loading FixMyStreet contacts data
 
-class Parsers::FmsContactsParser 
+class Parsers::FmsContactsParser
 
   def csv_options
-    { :quote_char => '"', 
-      :col_sep => ",", 
-      :row_sep =>:auto, 
+    { :quote_char => '"',
+      :col_sep => ",",
+      :row_sep =>:auto,
       :return_headers => false,
       :headers => :first_row,
       :encoding => 'N' }
   end
-  
+
   def parse_contacts filepath
     csv_data = File.read(filepath)
     FasterCSV.parse(csv_data, csv_options) do |row|
       next if row['deleted'] == 't'
+      next if row['category'].blank?
       confirmed = !row['confirmed'].nil?
-      yield CouncilContact.new({:area_id => row['area_id'], 
-                                :email => row['email'], 
-                                :confirmed => confirmed,
-                                :category => row['category'],
-                                :notes => row['note']})
+      if row['area_id'] == '2225' and row['email'].strip == 'SPECIAL'
+        district_mappings = { 'eastarea' => [2315, 2312],
+                              'midarea' => [2317, 2314, 2316],
+                              'southarea' => [2319, 2320, 2310],
+                              'westarea' => [2309, 2311, 2318, 2313] }
+        district_mappings.each do |area_name, district_ids|
+          district_ids.each do |district_id|
+            yield CouncilContact.new({:area_id => row['area_id'],
+                                      :email => "highways.#{area_name}@essexcc.gov.uk",
+                                      :confirmed => confirmed,
+                                      :district_id => district_id,
+                                      :category => row['category'].strip,
+                                      :notes => row['note']})
+          end
+        end
+      else
+        yield CouncilContact.new({:area_id => row['area_id'],
+                                  :email => row['email'].strip,
+                                  :confirmed => confirmed,
+                                  :category => row['category'].strip,
+                                  :notes => row['note']})
+      end
     end
   end
-  
+
 end
