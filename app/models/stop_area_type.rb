c5716d9411da9ce33d522b18ed331bf08e0308c6
Louise Crow <louise.crow@gmail.com>
2011-03-31 15:14:43 +0100

    Remove trailing spaces.

--- app/models/stop_area_type.rb
+++ app/models/stop_area_type.rb
@@ -20,23 +20,23 @@ class StopAreaType < ActiveRecord::Base
   @@ferry_terminal_types = ['GFTD']
   @@primary_types = @@station_types + @@ferry_terminal_types + @@bus_station_types
   @@atomic_types = @@station_types + @@ferry_terminal_types
-  cattr_accessor :station_types, 
-                 :ferry_terminal_types, 
-                 :bus_station_types, 
-                 :primary_types, 
+  cattr_accessor :station_types,
+                 :ferry_terminal_types,
+                 :bus_station_types,
+                 :primary_types,
                  :atomic_types
-  
+
   def self.codes
-    connection.select_rows("SELECT DISTINCT description, code 
-                            FROM stop_area_types 
+    connection.select_rows("SELECT DISTINCT description, code
+                            FROM stop_area_types
                             ORDER BY code")
   end
-  
+
   def self.codes_for_transport_mode(transport_mode_id)
-    calculate_hashes if @@codes_by_mode.empty? 
+    calculate_hashes if @@codes_by_mode.empty?
     @@codes_by_mode[transport_mode_id]
   end
-  
+
   def self.calculate_hashes
     transport_mode_stop_area_types = TransportModeStopAreaType.find(:all, :include => :stop_area_type)
     transport_mode_stop_area_types.each do |tmst|
@@ -50,12 +50,12 @@ class StopAreaType < ActiveRecord::Base
       @@modes_by_code[tmst.stop_area_type.code] << tmst.transport_mode_id
     end
   end
-  
+
   def self.transport_modes_for_code(code)
     calculate_hashes if @@modes_by_code.empty?
     @@modes_by_code[code]
   end
-  
+
   def self.conditions_for_transport_mode(transport_mode_id)
     if transport_mode_id == TransportMode.find_by_name("Tram/Metro").id
       codes = ['GTMU']
@@ -64,5 +64,5 @@ class StopAreaType < ActiveRecord::Base
     end
     ['area_type in (?)', [codes]]
   end
-  
+
 end
