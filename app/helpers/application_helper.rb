b43e643037409e1b73faec1b4e0cf9d493705ecd
Louise Crow <louise.crow@gmail.com>
2011-04-07 08:59:40 +0100

    Sorting links need to point to admin urls.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -366,6 +366,6 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, params.merge({:sort => column, :direction => direction}), {:class => css_class}
+    link_to title, admin_url(url_for(params.merge({:sort => column, :direction => direction}))), {:class => css_class}
   end
 end
