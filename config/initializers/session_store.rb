5b4264380e336c974d04a165e034524de646ce9e
Louise Crow <louise.crow@gmail.com>
2011-04-14 20:18:53 +0100

    Add a bit of middleware to prevent responses setting a session cookie when there is no data to store in it.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,6 +4,7 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
+
 ActionController::Base.session = {
   :key => '_fixmytransport_session',
   :secret => MySociety::Config.get("COOKIE_STORE_SESSION_SECRET", 'this default is insecure as code is open source, please override for live sites in config/general; this will do for local development')
@@ -12,3 +13,7 @@ ActionController::Base.session = {
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
 ActionController::Base.session_store = :active_record_store
+
+# Insert a bit of middleware code to prevent uneeded cookie setting.
+require 'lib/fixmytransport/strip_empty_sessions'
+ActionController::Dispatcher.middleware.insert_before ActionController::Base.session_store, FixMyTransport::StripEmptySessions, :key => '_fixmytransport_session', :path => "/", :httponly => true
