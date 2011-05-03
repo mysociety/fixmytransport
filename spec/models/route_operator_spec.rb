917db432b447952bef2a5c013a1091f8a1d8b897
Louise Crow <louise.crow@gmail.com>
2011-05-03 14:35:14 +0100

    On creation of an association between an operator and a route, assign the operator to any problems on the route that haven't been sent and don't have an operator. On destruction of a route operator association, throw an exception if any problems exist that associate that operator with that route.

--- spec/models/route_operator_spec.rb
+++ spec/models/route_operator_spec.rb
@@ -13,15 +13,47 @@
 require 'spec_helper'
 
 describe RouteOperator do
+
   before(:each) do
+    @mock_route = mock_model(Route)
+    @mock_operator = mock_model(Operator)
+    @mock_problem = mock_model(Problem)
     @valid_attributes = {
-      :operator_id => 1,
-      :route_id => 1
+      :operator => @mock_operator,
+      :route => @mock_route
     }
   end
+  
+  describe 'when creating a new route operator' do
 
-  it "should create a new instance given valid attributes" do
-    route_operator = RouteOperator.new(@valid_attributes)
-    route_operator.valid?.should be_true
+    it "should create a new instance given valid attributes" do
+      route_operator = RouteOperator.new(@valid_attributes)
+      route_operator.valid?.should be_true
+    end
+    
+    it "should assign its operator to any unsent problems for this route that don't have an operator" do 
+      conditions = ['location_type = ? AND location_id = ? AND operator_id is NULL', 'Route', @mock_route]
+      Problem.stub!(:unsent).and_return([])
+      Problem.unsent.should_receive(:find).with(:all, :conditions => conditions).and_return([@mock_problem])
+      @mock_problem.should_receive(:operator=).with(@mock_operator)
+      @mock_problem.should_receive(:save!)
+      route_operator = RouteOperator.new(@valid_attributes)
+      route_operator.update_problems()
+    end
+    
+  end
+
+  describe 'when destroying a route operator' do 
+    
+    it 'should raise an exception if problems exist with that route and operator' do 
+      route_operator = RouteOperator.new(@valid_attributes)
+      puts route_operator.inspect
+      conditions = ['location_type = ? AND location_id = ? AND operator_id = ?', 
+                    'Route', route_operator.route.id, route_operator.operator.id]
+      Problem.should_receive(:find).with(:all, :conditions => conditions).and_return([@mock_problem])
+      expected_error_message = "Cannot destroy association of route #{@mock_route.id} with operator #{@mock_operator.id} - problems need updating"
+      lambda{ route_operator.check_problems() }.should raise_error(expected_error_message)
+    end
+    
   end
 end
