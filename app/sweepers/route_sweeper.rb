7df2ecec60052fe67fcab84604d516a6fcbd501f
Louise Crow <louise.crow@gmail.com>
2011-05-03 12:39:56 +0100

    Fixing cache sweeping for routes - it wasn't working when the admin interface was proxied via a secure server as the controller that is changing the routes is using admin urls, but the cache paths that need to be swept are using main urls.

--- app/sweepers/route_sweeper.rb
+++ app/sweepers/route_sweeper.rb
@@ -1,31 +1,38 @@
 class RouteSweeper < ActionController::Caching::Sweeper
-  
+
   observe :route
-  
+
   def after_create(route)
     expire_cache_for(route)
   end
-  
+
   def after_destroy(route)
     expire_cache_for(route)
   end
-  
+
   def after_update(route)
     expire_cache_for(route)
   end
-  
+
   private
-  
+
   def expire_cache_for(route)
     # note the controller leading slash to prevent any namespacing of the controller
-    expire_fragment(:controller => '/locations', 
-                    :action => 'show_route_region', 
-                    :id => route.region)
+    # also we need to explicitly specify that we want to clear the cache path with the main url
+    # in it, as this code may be called from an admin controller via some proxied url
+    route_path = url_for(:controller => '/locations',
+                         :action => 'show_route_region',
+                         :id => route.region,
+                         :only_path => true)                     
+    expire_fragment(main_url(route_path, { :skip_protocol => true }))
     if route.previous_version and (route.previous_version.region_id != route.region_id)
-      expire_fragment(:controller => '/locations', 
-                      :action => 'show_route_region', 
-                      :id => route.previous_version.region)
-      end
+      route_path = url_for(:controller => '/locations',
+                           :action => 'show_route_region',
+                           :id => route.previous_version.region,
+                           :only_path => true)
+      expire_fragment(main_url(route_path, { :skip_protocol => true }))
+    end
   end
-  
+
+
 end
