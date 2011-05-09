23ed7a1380cc4109d016f5636149c9809750b9aa
Louise Crow <louise.crow@gmail.com>
2011-05-09 12:34:17 +0100

    Bugfix paths

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   before_save :generate_email_local_part, :unless => :unregistered?
   has_attached_file :profile_photo,
                     :path => "#{MySociety::Config.get('FILE_DIRECTORY', ':rails_root/public/system')}/paperclip/:class/:attachment/:id/:style/:filename",
-                    :url => "#{MySociety::Config.get('PAPERCLIP_URL_BASE', '/system')}/paperclip/:class/:attachment/:id/:style/:filename",
+                    :url => "#{MySociety::Config.get('PAPERCLIP_URL_BASE', '/system/paperclip')}/:class/:attachment/:id/:style/:filename",
                     :default_url => "/images/paperclip_defaults/:class/:attachment/missing_:style.png",
                     :styles => { :large_thumb => "70x70#",
                                  :small_thumb => "40x40#" }
