700a2fa839a00cfedd2e65b056042c3835ac2d49
Louise Crow <louise.crow@gmail.com>
2011-05-11 17:23:28 +0100

    Make login redirects a bit nicer.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,8 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
+  before_filter :save_redirect
 
   def new
-    if params[:redirect] and params[:redirect].starts_with('/')
-      session[:return_to] = params[:redirect]
-    end
     @user_session = UserSession.new
   end
 
@@ -26,6 +24,14 @@ class UserSessionsController < ApplicationController
     current_user_session.destroy
     flash[:notice] = t(:logout_successful)
     redirect_back_or_default login_path
+  end
+  
+  private
+  
+  def save_redirect
+    if params[:redirect] and params[:redirect].starts_with?('/')
+      session[:return_to] = params[:redirect]
+    end
   end
 
 end
