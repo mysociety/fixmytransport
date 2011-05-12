821400b0529124b001b95724bb4df2d734da6b8f
Louise Crow <louise.crow@gmail.com>
2011-05-12 11:08:56 +0100

    Bugfix - change instance variable name to avoid conflict when using account creation popup over profile page.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -20,26 +20,26 @@ class AccountsController < ApplicationController
   end
   
   def new
-    @user = User.new
+    @account_user = User.new
   end
   
   def create
-    @user = User.find_or_initialize_by_email(params[:user][:email])
-    already_registered = @user.registered?
-    @user.ignore_blank_passwords = false
+    @account_user = User.find_or_initialize_by_email(params[:user][:email])
+    already_registered = @account_user.registered?
+    @account_user.ignore_blank_passwords = false
     # want to force validation of passwords
-    @user.registered = true
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
-    if @user.valid? 
+    @account_user.registered = true
+    @account_user.name = params[:user][:name]
+    @account_user.email = params[:user][:email]
+    @account_user.password = params[:user][:password]
+    @account_user.password_confirmation = params[:user][:password_confirmation]
+    if @account_user.valid? 
       # no one's used this email before, or someone has, but not registered
-      if @user.new_record? or ! already_registered
+      if @account_user.new_record? or ! already_registered
         # don't want to actually set them as registered until they confirm
-        @user.registered = false
-        @user.save_without_session_maintenance
-        @user.deliver_new_account_confirmation!
+        @account_user.registered = false
+        @account_user.save_without_session_maintenance
+        @account_user.deliver_new_account_confirmation!
         @action = t(:your_account_wont_be_created)
         render :template => 'shared/confirmation_sent'
       else
@@ -47,8 +47,8 @@ class AccountsController < ApplicationController
         # response in browser should look the same in case this is someone
         # just trying to find out if there's an account for an email address that's
         # not theirs. Refresh the user, discard all the changes
-        @user = User.find_or_initialize_by_email(params[:user][:email])
-        @user.deliver_already_registered!
+        @account_user = User.find_or_initialize_by_email(params[:user][:email])
+        @account_user.deliver_already_registered!
         @action = t(:your_account_wont_be_created)
         render :template => 'shared/confirmation_sent'
       end
@@ -59,9 +59,9 @@ class AccountsController < ApplicationController
   
   def confirm
     # set the user as registered, save and log in
-    @user.registered = true
-    @user.save
-    UserSession.create(@user, remember_me=false) # Log user in manually
+    @account_user.registered = true
+    @account_user.save
+    UserSession.create(@account_user, remember_me=false) # Log user in manually
     flash[:notice] = t(:successfully_confirmed_account)
     redirect_back_or_default root_url
   end
@@ -70,8 +70,8 @@ class AccountsController < ApplicationController
   
   def load_user_using_perishable_token 
     # not currently using a timeout on the tokens
-    @user = User.find_using_perishable_token(params[:email_token], token_age=0)  
-    unless @user
+    @account_user = User.find_using_perishable_token(params[:email_token], token_age=0)  
+    unless @account_user
       flash[:notice] = t(:could_not_find_account)
       redirect_to root_url  
     end
