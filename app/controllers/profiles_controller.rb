0d3ca43df7979a73b2c3e8f7db129134b968cb53
Louise Crow <louise.crow@gmail.com>
2011-06-01 09:13:03 +0100

    Showing profiles only for users who have logged in successfully.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,14 @@
 class ProfilesController < ApplicationController
 
-  before_filter :require_registered_user, :only => [:show]
+  before_filter :require_profile_user_who_has_logged_in, :only => [:show]
   
   def show
   end
 
   private
   
-  def require_registered_user
-    @user = User.find(params[:id], :conditions => ['registered = ?', true])
+  def require_profile_user_who_has_logged_in
+    @user = User.find(params[:id], :conditions => ['login_count > 0'])
     if ! @user
       render :file => "#{RAILS_ROOT}/public/404.html", :status => :not_found
       return false
