6b517c691eb0a4c3291dcb283c6c875fd5b29fa5
Louise Crow <louise.crow@gmail.com>
2011-06-06 10:45:36 +0100

    Specs for customized emails for lazy registration

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,7 +4,8 @@ class UserMailer < ApplicationMailer
     subject "[FixMyTransport] Password reset instructions"  
     from contact_from_name_and_email
     recipients user.email  
-    body :edit_password_reset_url => main_url(edit_password_reset_path(user.perishable_token)), :user => user
+    body :edit_password_reset_url => main_url(edit_password_reset_path(user.perishable_token)), 
+         :user => user
   end
 
   def new_account_confirmation(user, post_login_action_data, unconfirmed_model)
