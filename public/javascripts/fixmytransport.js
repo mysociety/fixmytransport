d5fbd63fd234cc696a55503d3713276904f767ff
Josh Angell <josh@supercooldesign.co.uk>
2011-06-15 15:54:07 +0100

    Removed a trailing comma from the js, might have been what was killing IE.

--- public/javascripts/fixmytransport.js
+++ public/javascripts/fixmytransport.js
@@ -446,7 +446,7 @@ $(document).ready(function(){
       imageLoading:  '/images/lightbox-ico-loading.gif',
      	imageBtnClose: '/images/lightbox-btn-close.gif',
      	imageBtnPrev:  '/images/lightbox-btn-prev.gif',
-     	imageBtnNext:  '/images/lightbox-btn-next.gif',
+     	imageBtnNext:  '/images/lightbox-btn-next.gif'
     });
   }
 
