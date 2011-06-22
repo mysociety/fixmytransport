741798853e08e881100ca038243e03a2672451bf
Louise Crow <louise.crow@gmail.com>
2011-06-22 15:49:40 +0100

    Strip the response.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -15,7 +15,7 @@ class ServicesController < ApplicationController
     require 'open-uri'
     gaze = MySociety::Config.get('GAZE_URL', '')
     if gaze != ''
-      render :text => open("#{gaze}/gaze-rest?f=get_country_from_ip;ip=#{request.remote_ip}").read
+      render :text => open("#{gaze}/gaze-rest?f=get_country_from_ip;ip=#{request.remote_ip}").read.strip
     else
       render :text => ''
     end
