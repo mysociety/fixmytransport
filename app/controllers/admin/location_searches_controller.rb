9cdfe5a00bebab83895d8b63e513ec19dac75dd7
Louise Crow <louise.crow@gmail.com>
2011-07-05 15:14:56 +0100

    Paginate location searches.

--- app/controllers/admin/location_searches_controller.rb
+++ app/controllers/admin/location_searches_controller.rb
@@ -1,7 +1,18 @@
 class Admin::LocationSearchesController < Admin::AdminController
   
   def index
-    @searches = LocationSearch.find(:all, :order => 'created_at desc')
+    @searches = WillPaginate::Collection.create((params[:page] or 1), 30) do |pager|
+      searches = LocationSearch.find(:all, :order => 'created_at desc',
+                                         :limit => pager.per_page,
+                                         :offset => pager.offset)
+      # inject the result array into the paginated collection:
+      pager.replace(searches)
+
+      unless pager.total_entries
+        # the pager didn't manage to guess the total count, do it manually
+        pager.total_entries = LocationSearch.count
+      end
+    end
   end
   
   def show
