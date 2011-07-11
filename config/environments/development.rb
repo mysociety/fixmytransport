9fcea4440eaef0eac64394433953ecfe99e56b6e
Louise Crow <louise.crow@gmail.com>
2011-07-11 19:35:18 +0100

    Set dev env to send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,5 +14,5 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.delivery_method = :test
+config.action_mailer.raise_delivery_errors = true
+
