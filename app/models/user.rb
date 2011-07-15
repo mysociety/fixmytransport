cf403c7e6fb121a7672a2299c151d8f581d7800c
Louise Crow <louise.crow@gmail.com>
2011-07-15 21:28:04 +0100

    Code for getting facebook profile images.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@
 #  updated_at        :datetime
 #  wants_fmt_updates :boolean
 #
+require 'open-uri'
+require 'net/http'
+require 'uri'
 
 class User < ActiveRecord::Base
   validates_presence_of :email
@@ -24,6 +27,8 @@ class User < ActiveRecord::Base
   has_many :initiated_campaigns, :foreign_key => :initiator_id, :class_name => 'Campaign'
   has_many :sent_emails, :as => :recipient
   has_many :access_tokens
+  before_validation :download_remote_profile_photo, :if => :profile_photo_url_provided?
+
   has_attached_file :profile_photo,
                     :path => "#{MySociety::Config.get('FILE_DIRECTORY', ':rails_root/public/system')}/paperclip/:class/:attachment/:id/:style/:filename",
                     :url => "#{MySociety::Config.get('PAPERCLIP_URL_BASE', '/system/paperclip')}/:class/:attachment/:id/:style/:filename",
@@ -32,15 +37,18 @@ class User < ActiveRecord::Base
                                  :small_thumb => "40x40#",
                                  :medium_thumb => "46x46#" }
 
-  attr_accessor :ignore_blank_passwords, :skip_email_uniqueness_validation
-  
+  attr_accessor :ignore_blank_passwords,
+                :skip_email_uniqueness_validation,
+                :profile_photo_url,
+                :error_on_bad_profile_photo_url
+
   has_friendly_id :name, :use_slug => true, :allow_nil => true
 
   named_scope :registered, :conditions => { :registered => true }
 
   acts_as_authentic do |c|
 
-    # moving from SHA512 to BCrypt - remove when done 
+    # moving from SHA512 to BCrypt - remove when done
     c.crypto_provider = Authlogic::CryptoProviders::BCrypt
     c.transition_from_crypto_providers = Authlogic::CryptoProviders::Sha512
 
@@ -64,7 +72,7 @@ class User < ActiveRecord::Base
   def password_not_required
     unregistered? or !access_tokens.empty?
   end
-  
+
   def unregistered?
     !registered
   end
@@ -87,12 +95,12 @@ class User < ActiveRecord::Base
     end
     return true
   end
-  
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     UserMailer.deliver_password_reset_instructions(self)
   end
-  
+
   def mark_seen(campaign)
     if current_supporter = self.campaign_supporters.confirmed.detect{ |supporter| supporter.campaign == campaign }
       if current_supporter.new_supporter?
@@ -101,11 +109,11 @@ class User < ActiveRecord::Base
       end
     end
   end
-  
+
   def supporter_or_initiator(campaign)
     return (self == campaign.initiator || campaign.supporters.include?(self))
   end
-  
+
   def new_supporter?(campaign)
     if current_supporter = self.campaign_supporters.confirmed.detect{ |supporter| supporter.campaign == campaign }
       if current_supporter.new_supporter?
@@ -114,13 +122,37 @@ class User < ActiveRecord::Base
     end
     return false
   end
-  
+
+  def download_remote_profile_photo
+    self.profile_photo = do_download_remote_profile_photo
+    self.profile_photo_remote_url = profile_photo_url
+  end
+
+  def do_download_remote_profile_photo
+    begin
+      io = open(URI.parse(profile_photo_url))
+      def io.original_filename; base_uri.path.split('/').last; end
+      io.original_filename.blank? ? nil : io
+    rescue
+      if self.error_on_bad_profile_photo_url
+        raise
+      end
+    end
+  end
+
+  def profile_photo_url_provided?
+    !self.profile_photo_url.blank?
+  end
+
   # class methods
-  
+
   def self.get_facebook_data(access_token)
-    require 'open-uri'
-    contents = open("https://graph.facebook.com/me?access_token=#{CGI::escape(access_token)}").read
-    facebook_data = JSON.parse(contents)
+    graph_api_url = MySociety::Config.get('FACEBOOK_GRAPH_API_URL', '')
+    if graph_api_url
+      query_string = "access_token=#{CGI::escape(access_token)}&fields=name,email,picture&type=large"
+      contents = open("#{graph_api_url}/me?#{query_string}").read
+      facebook_data = JSON.parse(contents)
+    end
   end
 
   def self.handle_external_auth_token(access_token, source, remember_me)
@@ -137,18 +169,154 @@ class User < ActiveRecord::Base
         user = User.find(:first, :conditions => ['email = ?', email])
         if not user
           user = User.new({:name => name, :email => email})
-        end   
+        end
         user.registered = true
+        # don't replace an existing uploaded photo
+        if ! user.profile_photo?
+          # discard the profile photo if it's just the default
+          if facebook_data['picture'] and !self.facebook_static_profile_picture?(facebook_data['picture'])
+            user.profile_photo_url = facebook_data['picture']
+            user.error_on_bad_profile_photo_url = false
+          end
+        end
         user.access_tokens.build({:user_id => user.id,
-                                    :token_type => 'facebook',
-                                    :key => fb_id,
-                                    :token => access_token})
+                                  :token_type => 'facebook',
+                                  :key => fb_id,
+                                  :token => access_token})
         user.save_without_session_maintenance
       end
       UserSession.create(user, remember_me=remember_me)
     end
   end
-  
+
+
+  def self.get_facebook_profile_picture_url(access_token)
+
+    if graph_api_url
+
+      contents = open("#{graph_api_url}/#{access_token.key}?#{query_string}").read
+      facebook_data = JSON.parse(contents)
+      if facebook_data['picture']
+        return facebook_data['picture']
+      end
+    end
+    return nil
+  end
+
+  def self.get_facebook_app_access_token(http_session, verbose)
+    facebook_app_id = MySociety::Config.get('FACEBOOK_APP_ID', '')
+    facebook_app_secret = MySociety::Config.get('FACEBOOK_APP_SECRET', '')
+    if ! facebook_app_id or !facebook_app_secret
+      raise "Missing Facebook app credentials"
+    end
+    auth_params = { :client_id => facebook_app_id,
+                    :client_secret => facebook_app_secret,
+                    :grant_type => 'client_credentials' }
+    app_auth_request = Net::HTTP::Get.new("/oauth/access_token?#{auth_params.to_param}")
+    app_auth_response = http_session.request(app_auth_request)
+    case app_auth_response
+    when Net::HTTPSuccess
+      response_body = app_auth_response.body
+      field, value = response_body.split("=")
+      if field != 'access_token'
+        raise "Unexpected response from Facebook app authentication: #{field}=#{value}"
+      end
+      access_token = value
+      puts "Got app access token: #{access_token}" if verbose
+    else
+      app_auth_response.error!
+    end
+    return access_token
+  end
+
+  def self.get_facebook_batch_api_data(http_session, fb_query_set, access_token, verbose)
+    profile_data_request = Net::HTTP::Post.new('/')
+    profile_data_request.body = "access_token=#{access_token}&batch=#{CGI.escape(fb_query_set.to_json)}"
+    puts "Asking for picture data on #{fb_query_set.size} users" if verbose
+    profile_picture_response = http_session.request(profile_data_request)
+    case profile_picture_response
+    when Net::HTTPSuccess
+      response_body = profile_picture_response.body
+      profile_picture_data = JSON.load(response_body)
+      puts "Got picture data" if verbose
+    else
+      profile_picture_response.error!
+    end
+    return profile_picture_data
+  end
+
+  def self.update_remote_profile_photos(verbose=false)
+    graph_api_url = MySociety::Config.get('FACEBOOK_GRAPH_API_URL', '')
+    facebook_batch_api_limit = 20
+    if graph_api_url
+
+      facebook_queries = []
+
+      # get a list of fb keys that we want to check profile pictures for
+      AccessToken.find_each(:conditions => ['token_type = ?', 'facebook'], :include => :user) do |access_token|
+        user = access_token.user
+        if ! user.profile_photo? or !user.profile_photo_remote_url.blank?
+          query_string = "fields=id,picture&type=large"
+          facebook_queries << {:method => 'GET', :relative_url => "#{access_token.key}?#{query_string}"}
+        end
+      end
+
+      # Set up an SSL session
+      graph_uri = URI.parse(graph_api_url)
+      http = Net::HTTP.new(graph_uri.host, graph_uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      # get an app access token
+      access_token = self.get_facebook_app_access_token(http, verbose)
+
+      # get the profile data
+      facebook_queries.each_slice(facebook_batch_api_limit) do |fb_query_set|
+        profile_picture_data = self.get_facebook_batch_api_data(http, fb_query_set, access_token, verbose)
+        profile_picture_data.each do |picture_response|
+          self.set_profile_remote_photo(picture_response['body'], verbose)
+        end
+      end
+    end
+  end
+
+  def self.set_profile_remote_photo(json_response, verbose)
+    picture_data = JSON.load(json_response)
+    picture_url =  picture_data['picture']
+    id = picture_data['id']
+    puts "FB key: #{id}, url #{picture_url}" if verbose
+    token = AccessToken.find(:first, :conditions => ['key = ? and token_type = ?', id, 'facebook'])
+    if !token
+      puts "No token for returned key #{id}" if verbose
+    else
+      user = token.user
+      puts "Looking at picture URL for user #{user.id}" if verbose
+      if picture_url == user.profile_photo_remote_url
+        puts "Remote profile picture URL for user #{user.id} is same as current" if verbose
+      else
+        if self.facebook_static_profile_picture?(picture_url)
+          puts "Remote profile picture URL for user #{user.id} is static #{picture_url}" if verbose
+        else
+          puts "Setting remote profile picture URL for user to #{picture_url}" if verbose
+          self.update_remote_profile_photo(user, picture_url)
+        end
+      end
+    end
+  end
+
+  def self.facebook_static_profile_picture?(url)
+    if /static/ =~ url
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.update_remote_profile_photo(user, photo_url)
+    user.profile_photo_url = photo_url
+    user.error_on_bad_profile_photo_url = true
+    user.save_without_session_maintenance
+  end
 
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,7 +62,7 @@ describe User do
       end
 
       it 'should parse the response as JSON' do
-        JSON.should_receive(:parse)
+        JSON.should_receive(:parse) 
         User.get_facebook_data('mytoken')
       end
         
@@ -73,16 +73,20 @@ describe User do
       before do 
         @mock_user = mock_model(User, :save! => true, 
                                       :access_tokens => [],
-                                      :registered= => true)
+                                      :registered= => true,
+                                      :profile_photo_url= => nil,
+                                      :profile_photo? => false,
+                                      :error_on_bad_profile_photo_url= => nil)
         @mock_user.access_tokens.stub!(:build).and_return(true)
         @mock_user.stub!(:save_without_session_maintenance)
         User.stub!(:new).and_return(@mock_user)
         User.stub!(:get_facebook_data).and_return({'id' => 'myfbid',
                                                    'name' => 'Test Name', 
-                                                   'email' => 'test@example.com'})
+                                                   'email' => 'test@example.com',
+                                                   'picture' => 'http://profile.example.com/mypicture.jpg'})
         UserSession.stub!(:create)
       end
-      
+    
       it 'should set the user as registered' do 
         @mock_user.should_receive(:registered=).with(true)
         User.handle_external_auth_token('mytoken', 'facebook', false)
@@ -92,10 +96,213 @@ describe User do
         AccessToken.should_receive(:find).with(:first, :conditions => ['key = ? and token_type = ?', 'myfbid', 'facebook'])
         User.handle_external_auth_token('mytoken', 'facebook', false)
       end
+            
+      describe 'when an access token does not exist' do 
+        
+        before do 
+          AccessToken.stub!(:find).and_return(nil)
+        end
       
+        it 'should set the remote profile photo url' do 
+          @mock_user.should_receive(:profile_photo_url=).with("http://profile.example.com/mypicture.jpg")
+          User.handle_external_auth_token('mytoken', 'facebook', false)
+        end
+        
+        it 'should set the user model to not error if there is an issue with the remote url' do 
+          @mock_user.should_receive(:error_on_bad_profile_photo_url=).with(false)
+          User.handle_external_auth_token('mytoken', 'facebook', false)
+        end
+        
+        it 'should not set the remote profile photo url if the remote url is a static one' do 
+          User.stub!(:get_facebook_data).and_return({'id' => 'myfbid',
+                                                     'name' => 'Test Name', 
+                                                     'email' => 'test@example.com',
+                                                     'picture' => 'http://static.example.com/mypicture.jpg'})
+          @mock_user.should_not_receive(:profile_photo_url=)
+          User.handle_external_auth_token('mytoken', 'facebook', false)
+        end
+      
+        it 'should not replace an existing uploaded profile photo with a facebook photo' do 
+          @mock_user.stub!(:profile_photo_remote_url).and_return(nil)
+          @mock_user.stub!(:profile_photo?).and_return(true)
+          User.handle_external_auth_token('mytoken', 'facebook', false)
+        end
+          
+      end
       
     end
     
+  end
+  
+  describe 'when updating remote profile photos' do 
+    
+    before do 
+      User.stub!(:get_facebook_app_access_token).and_return('app_token')
+      User.stub!(:get_facebook_batch_api_data)
+      @mock_http = mock('http session', :use_ssl= => nil, :verify_mode= => nil)
+      Net::HTTP.stub!(:new).and_return(@mock_http)
+    end
+
+    
+    describe 'when a user has a facebook access token and no profile photo' do
+    
+      before do 
+        @user_without_profile_photo = mock_model(User, :profile_photo? => false,
+                                                       :profile_photo_url= => nil,
+                                                       :profile_photo_remote_url => nil,
+                                                       :save_without_session_maintenance => true,
+                                                       :error_on_bad_profile_photo_url= => nil)
+        @token_for_user_without_profile_photo = mock_model(AccessToken, :user => @user_without_profile_photo,
+                                                                        :key => 'mykey')
+        AccessToken.stub!(:find_each).and_yield(@token_for_user_without_profile_photo)
+        AccessToken.stub!(:find).and_return(@token_for_user_without_profile_photo)
+        picture_data = {'id' => 'mykey', 'picture' => 'http://example.com/image.jpg'}
+        User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+      end
+      
+      it 'should get an app facebook access token' do
+        User.should_receive(:get_facebook_app_access_token)
+        User.update_remote_profile_photos
+      end
+
+      it "should ask facebook for the user's profile picture" do 
+        expected_batch_request = [{:method=>"GET", :relative_url=>"mykey?fields=id,picture&type=large"}]
+        User.should_receive(:get_facebook_batch_api_data).with(@mock_http, 
+                                                               expected_batch_request, 
+                                                               'app_token', 
+                                                               false)
+        User.update_remote_profile_photos
+      end
+      
+      describe 'if the new url is static' do 
+        
+        before do 
+          picture_data = {'id' => 'mykey', 'picture' => 'http://static.example.com/image.jpg'}
+          User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+        end
+        
+        it 'should not set the remote profile url' do 
+          @user_without_profile_photo.should_not_receive(:profile_photo_url=)
+          User.update_remote_profile_photos
+        end
+         
+      end
+    
+      describe 'if the picture url from facebook is not static' do
+        
+        before do 
+          picture_data = {'id' => 'mykey', 'picture' => 'http://example.com/image.jpg'}
+          User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+        end
+      
+        it 'should set the user to error on a bad remote profile photo url' do 
+          @user_without_profile_photo.should_receive(:error_on_bad_profile_photo_url=).with(true)
+          User.update_remote_profile_photos
+        end
+        
+        it 'should set the remote profile url' do 
+          @user_without_profile_photo.should_receive(:profile_photo_url=).with('http://example.com/image.jpg')
+          User.update_remote_profile_photos
+        end
+        
+        it 'should save the user' do 
+          @user_without_profile_photo.should_receive(:save_without_session_maintenance)
+          User.update_remote_profile_photos
+        end
+    
+      end
+      
+    end
+    
+    describe 'when a user has a profile photo' do
+      
+      before do 
+        @user_with_profile_photo = mock_model(User, :profile_photo? => true,
+                                                    :save_without_session_maintenance => true,
+                                                    :error_on_bad_profile_photo_url= => nil,
+                                                    :profile_photo_url= => nil)
+        @token_for_user_with_profile_photo = mock_model(AccessToken, :user => @user_with_profile_photo,
+                                                                     :key => 'mykey')
+        AccessToken.stub!(:find_each).and_yield(@token_for_user_with_profile_photo)
+        AccessToken.stub!(:find).and_return(@token_for_user_with_profile_photo)
+        picture_data = {'id' => 'mykey', 'picture' => 'http://example.com/image.jpg'}
+        User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+      end
+      
+      describe 'when the profile photo is not remote' do
+        
+        before do 
+          @user_with_profile_photo.stub!(:profile_photo_remote_url).and_return(nil)
+        end
+        
+        it 'should not set the profile photo url' do 
+          @user_with_profile_photo.should_not_receive(:profile_photo_url=)
+          User.update_remote_profile_photos
+        end
+        
+      end
+      
+      describe 'if the profile photo is remote' do 
+        
+        before do 
+          @user_with_profile_photo.stub!(:profile_photo_remote_url).and_return('http://example.com/image.jpg')
+        end
+      
+        describe 'if the url from facebook is different from the existing url and not static' do 
+          
+          before do 
+            picture_data = {'id' => 'mykey', 'picture' => 'http://different.example.com/image.jpg'}
+            User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+          end
+          
+          it 'should set the user to error on a bad remote profile photo url' do 
+            @user_with_profile_photo.should_receive(:error_on_bad_profile_photo_url=).with(true)
+            User.update_remote_profile_photos()
+          end
+          
+          it 'should set the remote profile url' do 
+            expected_url = 'http://different.example.com/image.jpg'
+            @user_with_profile_photo.should_receive(:profile_photo_url=).with(expected_url)
+            User.update_remote_profile_photos()
+          end
+
+          it 'should save the user' do 
+            @user_with_profile_photo.should_receive(:save_without_session_maintenance)
+            User.update_remote_profile_photos()
+          end
+          
+        end 
+        
+        describe 'if the new url is static' do 
+          
+          before do 
+            picture_data = {'id' => 'mykey', 'picture' => 'http://static.example.com/image.jpg'}
+            User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+          end
+          
+          it 'should not set the remote profile url' do 
+            @user_with_profile_photo.should_not_receive(:profile_photo_url=)
+            User.update_remote_profile_photos()
+          end
+           
+        end
+        
+        describe "if the existing url is the same as the user's remote profile url" do 
+          
+          before do 
+            picture_data = {'id' => 'mykey', 'picture' => 'http://example.com/image.jpg'}
+            User.stub!(:get_facebook_batch_api_data).and_return([{'body' => picture_data.to_json}])
+          end
+            
+          it 'should not set the remote profile url' do 
+            @user_with_profile_photo.should_not_receive(:profile_photo_url=)
+            User.update_remote_profile_photos()
+          end
+        
+        end
+      end
+    
+    end
   end
 
 end
