15075f038a6f46b4870fca35c0b70fb12f8e3ea2
Louise Crow <louise.crow@gmail.com>
2011-07-18 11:54:30 +0100

    Bugfix - stop location type being set erroneously from the admin form for operator contacts.

--- app/controllers/admin/operator_contacts_controller.rb
+++ app/controllers/admin/operator_contacts_controller.rb
@@ -11,6 +11,10 @@ class Admin::OperatorContactsController < Admin::AdminController
   
   def create 
     @operator_contact = OperatorContact.new(params[:operator_contact])
+    # don't set location type without location id
+    if @operator_contact.location_id.blank? && ! @operator_contact.location_type.blank?
+      @operator_contact.location_type = nil
+    end
     if @operator_contact.save
       flash[:notice] = t('admin.operator_contact_created')
       redirect_to(admin_url(admin_operator_contact_path(@operator_contact)))
@@ -22,6 +26,10 @@ class Admin::OperatorContactsController < Admin::AdminController
   
   def update
     @operator_contact = OperatorContact.find(params[:id])
+    # don't set the stop_area_id unless it has a value
+    if params[:operator_contact][:stop_area_id].blank?
+      params[:operator_contact].delete(:stop_area_id)
+    end
     if @operator_contact.update_attributes(params[:operator_contact])
       flash[:notice] = t('admin.operator_contact_updated')
       redirect_to admin_url(admin_operator_contact_path(@operator_contact))
