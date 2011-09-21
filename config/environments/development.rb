6cad72dbfec06b91a5edb0e966aaad5a42a78d42
Louise Crow <louise.crow@gmail.com>
2011-09-21 12:14:49 +0100

    Allow a config setting to specify sending mail via test and not raising errors.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,5 +13,8 @@ config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = true
+# Send via test method
+if (MySociety::Config.getbool("NO_MAIL", false))
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
+end
