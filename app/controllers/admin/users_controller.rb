ba74e47ba1d95935bd40c06b65a6c28011d566f2
Dave Whiteland <dave@mysociety.org>
2011-10-12 01:38:47 +0100

    added admin page for editing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,4 +9,46 @@ class Admin::UsersController < Admin::AdminController
                                ORDER by comment_count desc")
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      msgs = []
+      @user.is_admin = params[:user][:is_admin] == '1'? true : false          # clunky i
+      @user.is_suspended = params[:user][:is_suspended] == '1'? true : false  # clunky ii
+      @user.is_expert = params[:user][:is_expert] == '1'? true : false        # clunky iii
+      if @user.is_admin_changed? 
+        if @user.is_admin
+          msgs << t('admin.user_now_administrator')
+        else
+          msgs << t('admin.user_now_not_admin')
+        end
+      end
+      if @user.is_suspended_changed?
+        if @user.is_suspended
+          msgs << t('admin.user_now_suspended')
+        else
+          msgs << t('admin.user_now_unsuspended')
+        end
+      end
+      if @user.is_expert_changed?
+        if @user.is_expert
+          msgs << t('admin.user_now_expert')
+        else
+          msgs << t('admin.user_now_normal_user')
+        end
+      end
+      @user.save!      
+      msgs << t('admin.user_updated')
+      flash[:notice] = msgs.join('<br/>')
+      redirect_to admin_url(admin_user_path(@user))
+    else
+      flash[:error] = t('admin.user_problem')
+      render :show
+    end
+  end
+  
 end
