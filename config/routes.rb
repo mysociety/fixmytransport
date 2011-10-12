64ab3799ac0c000f2c088b1422eca6d1944187f0
Dave Whiteland <dave@mysociety.org>
2011-10-12 17:44:01 +0100

    move old user list into new place as league of commenters

--- config/routes.rb
+++ config/routes.rb
@@ -187,6 +187,9 @@ ActionController::Routing::Routes.draw do |map|
                                                  :action => 'autocomplete_for_name'
     admin.connect "/autocomplete_for_locality_name", :controller => 'localities',
                                                  :action => 'autocomplete_for_name'
+
+    map.comment_league '/admin/comment_league', :action => 'comment_league', :controller => 'admin/users'
+
   end
 
 end
