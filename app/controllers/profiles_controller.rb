4040e777febff6c7547ac9a9fd28862b4ede9819
Louise Crow <louise.crow@gmail.com>
2011-10-24 11:08:50 +0100

    Allow user accounts to be hidden.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,7 @@ class ProfilesController < ApplicationController
   
   def require_profile_user_who_has_logged_in
     @user = User.find(params[:id], :conditions => ['login_count > 0'])
-    if ! @user
+    if (! @user) || @user.is_hidden?
       render :file => "#{RAILS_ROOT}/public/404.html", :status => :not_found
       return false
     end
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -11,7 +11,8 @@ describe ProfilesController do
     describe 'if the user whose profile is requested is not registered' do 
 
       before do 
-        @user = mock_model(User, :registered? => false)
+        @user = mock_model(User, :registered? => false,
+                                 :is_hidden? => false)
         User.should_receive(:find).with('55', :conditions => ['login_count > 0']).and_return(nil)
       end
     
@@ -26,7 +27,8 @@ describe ProfilesController do
       
       before do 
         @user = mock_model(User, :registered? => true,
-                                 :name => "Test User")
+                                 :name => "Test User",
+                                 :is_hidden? => false)
         User.should_receive(:find).with('55', :conditions => ['login_count > 0']).and_return(@user)
       end
       
@@ -35,24 +37,48 @@ describe ProfilesController do
       end
 
     end
+    
+    describe 'if the user whose profile is requested is hidden' do 
+    
+      before do 
+        @user = mock_model(User, :is_hidden? => true,
+                                 :login_count => 3)
+        User.stub!(:find).and_return(@user)
+      end
+      
+      it 'should return a 404' do 
+        make_request
+        response.status.should == '404 Not Found'
+      end
+      
+    end
 
-    # describe 'if the user whose profile is requested is suspended' do 
-    # 
-    #   before do 
-    #     @user = mock_model(User, :registered? => true,
-    #                              :suspended? => true,
-    #                              :suspended_reason => "User used too many rude words",
-    #                              :suspended_hide_contribs => false,
-    #                              :name => "Test User")
-    #     User.should_receive(:find).with('55', :conditions => ['login_count > 0']).and_return(@user)
-    #   end
-    #   
-    #   it 'should display the suspended notice and the reason for suspension' do 
-    #     make_request
-    #     response.should have_tag("p#user_suspended")
-    #   end
-    #       
-    # end
+    describe 'if the user whose profile is requested is suspended' do 
+      
+      integrate_views
+      
+      before do 
+        @user = mock_model(User, :registered? => true,
+                                 :suspended? => true,
+                                 :is_hidden? => false,
+                                 :suspended_reason => "User used too many rude words",
+                                 :suspended_hide_contribs => false,
+                                 :name => "Test User",
+                                 :profile_photo => mock('profile photo', :url => 'http://www.example.com'),
+                                 :location => nil,
+                                 :bio => nil, 
+                                 :initiated_campaigns => mock('initiated campaigns', :visible => []),
+                                 :campaigns => mock('campaigns', :visible => []),
+                                 :problems => mock('problems', :visible => []))
+        User.should_receive(:find).with('55', :conditions => ['login_count > 0']).and_return(@user)
+      end
+      
+      it 'should display the suspended notice and the reason for suspension' do 
+        make_request
+        response.should have_tag("div.user-suspended")
+      end
+          
+    end
         
   end
 
