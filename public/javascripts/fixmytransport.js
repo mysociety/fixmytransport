5079746efda721ebcbeb56caabd597f5570c7aa8
Louise Crow <louise.crow@gmail.com>
2011-11-16 09:38:35 +0000

    Add comment button at bottom of comment thread.

--- public/javascripts/fixmytransport.js
+++ public/javascripts/fixmytransport.js
@@ -388,7 +388,7 @@ $(document).ready(function(){
                   .attr('name', 'last_thread_index')
                   .attr('class', 'last_thread_index')
                   .val(last_thread_index));
-    	$("#login-box").dialog({title: $('.comment-trigger').attr('data-title')+":"});
+  	  $("#login-box").dialog({title: $(this).attr('data-title')+":"});
     	$("#login-box").dialog("open");
     	return false;
 	  }
