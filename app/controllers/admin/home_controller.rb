2c15f15c2e082331daf85b5cbc60739e56a35fa8
Dave Whiteland <dave@mysociety.org>
2012-01-02 01:12:25 +0000

    user count summary on admin index

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -8,6 +8,10 @@ class Admin::HomeController < Admin::AdminController
     @assignments_needing_attention = Assignment.find_need_attention(:limit => 10)
     @assignments_needing_attention_total = Assignment.count_need_attention
     @incoming_messages_without_campaign = IncomingMessage.find(:all, :conditions => ['campaign_id is null'], :limit => 10)
+    @user_count = User.registered.count()
+    @user_count_this_week = User.registered.count({:conditions =>  ['created_at >= ?', (Time.now - 7.days)]})
+    @comment_count = Comment.visible.count()
+    @comment_count_this_week = Comment.visible.count({:conditions =>  ['created_at >= ?', (Time.now - 7.days)]})
   end
   
 end
