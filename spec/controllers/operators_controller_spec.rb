f67fa0c5e354268424a73f41a16c1c9c27a0a7cd
Dave Whiteland <dave@mysociety.org>
2012-01-11 14:38:58 +0000

    better tests for the operator tabs

--- spec/controllers/operators_controller_spec.rb
+++ spec/controllers/operators_controller_spec.rb
@@ -43,5 +43,65 @@ describe OperatorsController do
     end
 
   end
+
+  describe 'GET #stations' do
+
+    def make_request(params={})
+      get :stations, params
+    end
+    
+    describe 'for an operator with one stop area' do
+
+      before do
+        StopArea.stub!(:find).and_return([mock_model(StopArea, :name => "A station", :area_type => "GRLS")])
+        Operator.stub!(:find).and_return(@mock_operator)
+      end    
+    
+      it 'should display the station in a tab and not retrieve any issues' do
+        StopArea.should_receive(:find)
+        Problem.should_not_receive(:find_recent_issues)
+        make_request(:id => "11")
+        assigns[:current_tab].should == :stations        
+      end
+
+      it 'should describe the stop areas as a station' do
+        make_request(:id => "11")
+        assigns[:banner_text].should include "station"
+        assigns[:station_type_descriptions][:short].should == "stations"
+      end
+
+      describe 'if that stop area is for ferries' do
+        
+        before do
+          StopArea.stub!(:find).and_return([mock_model(StopArea, :name => "Some jetty", :area_type => "GFTD")])
+        end
+        
+        it 'should describe it as a ferry terminal' do
+          make_request(:id => "11")
+          assigns[:banner_text].should include "ferry terminal"
+          assigns[:station_type_descriptions][:short].should == "terminals"
+        end
+      end
+      
+    end
+
+    describe 'for an operator with no stop areas' do
+
+      before do
+        StopArea.stub!(:find).and_return([])
+      end    
+    
+      it 'should retrieve issues instead, for the issues tab' do
+        Operator.should_receive(:find).with('11').and_return(@mock_operator)
+        StopArea.should_receive(:find)
+        Problem.should_receive(:find_recent_issues)
+        make_request(:id => "11")
+        assigns[:current_tab].should == :issues
+        assigns[:banner_text].should include "is not responsible for any stations or terminals"
+      end
+
+    end
+
+  end
   
 end
