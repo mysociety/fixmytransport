038b46a3718665e8c7c0da58c0be360c4edccfbe
Mark Longair <mhl@pobox.com>
2012-03-29 10:42:39 +0100

    Make sure there is only one contact per council and category

--- spec/models/council_contact_spec.rb
+++ spec/models/council_contact_spec.rb
@@ -11,7 +11,27 @@ describe CouncilContact do
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    CouncilContact.create!(@valid_attributes)
+  after(:each) do
+    CouncilContact.delete_all :area_id => 1
   end
+
+  it "should create a new instance given valid attributes" do
+    council_contact = CouncilContact.new @valid_attributes
+    council_contact.should be_valid
+  end
+
+  it "should only allow one email address for a particular operator and category" do
+    existing_council_contact = CouncilContact.create! @valid_attributes
+    new_council_contact = CouncilContact.new @valid_attributes
+    new_council_contact.should_not be_valid
+  end
+
+  it "should allow another address for a different category" do
+    existing_council_contact = CouncilContact.create! @valid_attributes
+    similar_attributes = @valid_attributes.clone
+    similar_attributes[:category] = "other"
+    new_council_contact = CouncilContact.new similar_attributes
+    new_council_contact.should be_valid
+  end
+
 end
