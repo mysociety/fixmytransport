ed5e4c50159141beddf54267e2ab3ae460fb4351
Louise Crow <louise.crow@gmail.com>
2012-04-25 11:14:29 +0100

    Format tweak

--- config/initializers/data_generations.rb
+++ config/initializers/data_generations.rb
@@ -1,4 +1,5 @@
-CURRENT_GENERATION = MySociety::Config.get("CURRENT_GENERATION", "1") 
+CURRENT_GENERATION = MySociety::Config.get("CURRENT_GENERATION", "1")
 PREVIOUS_GENERATION = CURRENT_GENERATION - 1
+
 require 'fixmytransport/data_generations'
 ActiveRecord::Base.send(:include, FixMyTransport::DataGenerations)
