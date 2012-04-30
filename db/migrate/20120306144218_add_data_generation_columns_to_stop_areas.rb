38f8c0cdb805e55f510994c6dc2d3846da5d87da
Louise Crow <louise.crow@gmail.com>
2012-04-30 11:18:46 +0100

    Try naming index to work around auto-generated name hitting postgres limit NAMEDATALEN

--- db/migrate/20120306144218_add_data_generation_columns_to_stop_areas.rb
+++ db/migrate/20120306144218_add_data_generation_columns_to_stop_areas.rb
@@ -3,19 +3,21 @@ class AddDataGenerationColumnsToStopAreas < ActiveRecord::Migration
     add_column :stop_areas, :generation_low, :integer
     add_column :stop_areas, :generation_high, :integer
     add_column :stop_areas, :previous_id, :integer
-    
+
     remove_index :stop_areas, :cached_slug
     execute("DROP INDEX index_stop_areas_on_code_lower")
     remove_index :stop_areas, :locality_id
     execute("DROP INDEX index_stop_areas_on_name_lower")
     remove_index :stop_areas, :primary_metaphone
     remove_index :stop_areas, :secondary_metaphone
-    
-    add_index :stop_areas, [:cached_slug, :generation_low, :generation_high]
-    execute("CREATE INDEX index_stop_areas_on_code_lower_and_gens 
+
+    add_index :stop_areas, [:cached_slug, :generation_low, :generation_high],
+              :name => 'index_stop_areas_on_cached_slug_and_gens'
+
+    execute("CREATE INDEX index_stop_areas_on_code_lower_and_gens
             ON stop_areas (lower(code), generation_low, generation_high);")
     add_index :stop_areas, [:locality_id, :generation_low, :generation_high]
-    execute("CREATE INDEX index_stop_areas_on_name_lower_and_gens 
+    execute("CREATE INDEX index_stop_areas_on_name_lower_and_gens
             ON stop_areas (lower(name), generation_low, generation_high);")
     add_index :stop_areas, [:primary_metaphone, :generation_low, :generation_high]
     add_index :stop_areas, [:secondary_metaphone, :generation_low, :generation_high]
@@ -33,6 +35,6 @@ class AddDataGenerationColumnsToStopAreas < ActiveRecord::Migration
             ON stop_areas (lower(name));")
     add_index :stop_areas, :primary_metaphone
     add_index :stop_areas, :secondary_metaphone
-    
+
   end
 end
