d369d5b4a2ca8a7782f439c4c8a6d999cf1355bb
Louise Crow <louise.crow@gmail.com>
2012-05-28 11:19:46 +0100

    Remove alter statement - column gone.

--- db/migrate/20120426142359_add_persistent_id_to_sub_routes.rb
+++ db/migrate/20120426142359_add_persistent_id_to_sub_routes.rb
@@ -9,8 +9,5 @@ class AddPersistentIdToSubRoutes < ActiveRecord::Migration
 
   def self.down
     remove_column :sub_routes, :persistent_id
-    execute("ALTER TABLE sub_routes
-             ALTER COLUMN persistent_id
-             DROP DEFAULT")
   end
 end
