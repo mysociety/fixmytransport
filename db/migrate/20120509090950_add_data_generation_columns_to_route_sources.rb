c145cbeaa92f357f00bc1ea8868d3294c02c2956
Louise Crow <louise.crow@gmail.com>
2012-06-18 11:33:16 +0100

    Naming index as autogenerated name is too long.

--- db/migrate/20120509090950_add_data_generation_columns_to_route_sources.rb
+++ db/migrate/20120509090950_add_data_generation_columns_to_route_sources.rb
@@ -4,11 +4,12 @@ class AddDataGenerationColumnsToRouteSources < ActiveRecord::Migration
     add_column :route_sources, :generation_high, :integer
     add_column :route_sources, :previous_id, :integer
     add_column :route_sources, :persistent_id, :integer
-    add_index :route_sources, [:route_id, :generation_low, :generation_high]
+    add_index :route_sources, [:route_id, :generation_low, :generation_high],
+                              :name => 'index_route_sources_on_route_id_and_gens'
   end
 
   def self.down
-    remove_index :route_sources, [:route_id, :generation_high, :generation_low]
+    remove_index :route_sources, :name => 'index_route_sources_on_route_id_and_gens'
     remove_column :route_sources, :generation_low
     remove_column :route_sources, :generation_high
     remove_column :route_sources, :previous_id
