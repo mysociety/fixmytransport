cb601f00d3ffe17374f5e93b1878ccc28ffc8f35
Louise Crow <louise.crow@gmail.com>
2012-07-03 16:28:32 +0100

    Migration to add loaded_at column to db

---
+++ db/migrate/20120703102302_add_loaded_at_to_data_generations.rb
@@ -0,0 +1,9 @@
+class AddLoadedAtToDataGenerations < ActiveRecord::Migration
+  def self.up
+    add_column :data_generations, :loaded_at, :datetime
+  end
+
+  def self.down
+    remove_column :data_generation, :loaded_at
+  end
+end
