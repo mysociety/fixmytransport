23dea43d565ff1e44b2192c8383f0e71b5764ff6
Louise Crow <louise.crow@gmail.com>
2013-01-09 10:58:26 +0000

    Upgrade to Rails 2.3.15, Rack 1.1.3

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -57,7 +57,7 @@ Rails::Initializer.run do |config|
   config.gem 'mime-types', :version => '1.17', :lib => 'mime/types' # Required by paperclip
   config.gem 'paper_trail', :version => '1.5.1'
   config.gem "paperclip", :version => "~> 2.7.0"
-  config.gem "rack", :version => '1.1.0'
+  config.gem "rack", :version => '1.1.3'
   config.gem 'rspec', :lib => false, :version => '1.3.1'
   config.gem 'rspec-rails', :lib => false, :version => '1.3.3'
   config.gem 'session', :version => '3.1.0'
