a41f725b1ea734dc97e8fcfde6f23bdc3ed41a8a
Louise Crow <louise.crow@gmail.com>
2013-02-02 15:03:24 +0000

    Load the mocks we need rather than trying to require classes from the Rails tests.

--- spec/initializers/actionmailer_smtp_format_patch_spec.rb
+++ spec/initializers/actionmailer_smtp_format_patch_spec.rb
@@ -1,6 +1,39 @@
 # Spec for patch in initializers/actionmailer_smtp_format_patch.rb
 require 'spec_helper'
-require "#{RAILS_ROOT}/vendor/rails/actionmailer/test/abstract_unit"
+
+def set_delivery_method(delivery_method)
+  @old_delivery_method = ActionMailer::Base.delivery_method
+  ActionMailer::Base.delivery_method = delivery_method
+end
+
+def restore_delivery_method
+  ActionMailer::Base.delivery_method = @old_delivery_method
+end
+
+class MockSMTP
+  def self.deliveries
+    @@deliveries
+  end
+
+  def initialize
+    @@deliveries = []
+  end
+
+  def sendmail(mail, from, to)
+    @@deliveries << [mail, from, to]
+  end
+
+  def start(*args)
+    yield self
+  end
+end
+
+class Net::SMTP
+  def self.new(*args)
+    MockSMTP.new
+  end
+end
+
 class TestMailer < ApplicationMailer
 
   def from_with_name
@@ -18,19 +51,19 @@ class TestMailer < ApplicationMailer
 end
 
 describe 'the patched version of actionmailer working with smtp' do
-    
-  before do 
+
+  before do
     set_delivery_method(:smtp)
   end
-  
+
   after do
     restore_delivery_method
   end
-  
+
   describe 'when delivering mail from an address without a name' do
- 
-    it 'should set the from address for the mail correctly' do 
-      
+
+    it 'should set the from address for the mail correctly' do
+
       TestMailer.deliver_from_without_name
       mail = MockSMTP.deliveries.first
       assert_not_nil mail
@@ -39,9 +72,9 @@ describe 'the patched version of actionmailer working with smtp' do
     end
   end
 
-  describe 'when delivering mail from an address with a name' do 
-    
-    it 'should set the from address for the mail correctly' do 
+  describe 'when delivering mail from an address with a name' do
+
+    it 'should set the from address for the mail correctly' do
       TestMailer.deliver_from_with_name
       mail = MockSMTP.deliveries.first
       assert_not_nil mail
@@ -49,7 +82,7 @@ describe 'the patched version of actionmailer working with smtp' do
 
       assert_equal 'system@localhost', from.to_s
     end
-    
+
   end
 
 end
